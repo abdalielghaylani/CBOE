ccdb7d8d58de78d9a7314c64287cf39cc968a71e
Churl Oh <churl.oh@perkinelmer.com>
2016-12-16 14:36:06 -0500

    Add dx-data-grid

--- subprojects/registration.client/src/main/webapp/app/app.module.ts
+++ subprojects/registration.client/src/main/webapp/app/app.module.ts
@@ -8,13 +8,13 @@ import {HttpModule} from '@angular/http';
 import {ProjectsComponent} from './project/projects.component';
 import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 import {ProjectService} from "./project/project.service";
-import {DxButtonModule} from 'devextreme-angular';
+import {DevExtremeModule} from 'devextreme-angular';
 import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.light.css';
 
 @NgModule({
     declarations: [WebApp, ProjectsComponent],
-    imports     : [BrowserModule, FormsModule, HttpModule, RouterModule.forRoot(routerConfig), DxButtonModule],
+    imports     : [BrowserModule, FormsModule, HttpModule, RouterModule.forRoot(routerConfig), DevExtremeModule],
     providers   : [ProjectService, {provide: LocationStrategy, useClass: HashLocationStrategy}],
     bootstrap   : [WebApp]
 })
--- subprojects/registration.client/src/main/webapp/app/project/projects.component.ts
+++ subprojects/registration.client/src/main/webapp/app/project/projects.component.ts
@@ -1,21 +1,22 @@
-import {Component, OnInit} from '@angular/core';
-import {Project} from './project.model';
-import {ProjectService} from './project.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Project } from './project.model';
+import { ProjectService } from './project.service';
 
 
 @Component({
     selector: 'projects',
     templateUrl: 'projects.component.html'
 })
-export class ProjectsComponent implements OnInit{
+export class ProjectsComponent implements OnInit {
     private projects: Project[];
 
-    constructor(private projectsService: ProjectService) {
+    constructor(private projectsService: ProjectService, private router: Router) {
     }
 
     ngOnInit() {
         this.projectsService.getProjects()
             .subscribe((projects: Project[]) => this.projects = projects,
-                error => console.error('ProjectsComponent: cannot get projects from ProjectService'));
+            error => console.error('ProjectsComponent: cannot get projects from ProjectService'));
     }
 }
