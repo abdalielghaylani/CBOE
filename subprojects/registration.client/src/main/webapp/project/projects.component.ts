89eea77f6c0bf5aea50721779d29dfcaf4a6e9a2
Churl Oh <churl.oh@perkinelmer.com>
2016-12-28 11:48:51 -0500

    Hide links

--- subprojects/registration.client/src/main/webapp/project/projects.component.ts
+++ subprojects/registration.client/src/main/webapp/project/projects.component.ts
@@ -19,4 +19,10 @@ export class ProjectsComponent implements OnInit {
             .subscribe((projects: Project[]) => this.projects = projects,
             error => console.error('ProjectsComponent: cannot get projects from ProjectService'));
     }
+
+    contentReady(e: any) {
+        var grid = e.component;
+        if (grid.columnCount() > 0 && grid.columnOption('_links', 'visible'))
+            grid.columnOption('_links', 'visible', false);
+    }
 }
