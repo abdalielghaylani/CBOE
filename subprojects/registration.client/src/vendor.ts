8445b76838a1ac1e0763fd63628f41abbd4e3649
Churl Oh <churl.oh@perkinelmer.com>
2016-12-30 08:46:23 -0500

    Remove unnecessary imported module

--- subprojects/registration.client/src/vendor.ts
+++ subprojects/registration.client/src/vendor.ts
@@ -14,6 +14,5 @@ require('atmosphere.js');
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import 'jquery/dist/jquery.min.js';
 import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.light.compact.css';
