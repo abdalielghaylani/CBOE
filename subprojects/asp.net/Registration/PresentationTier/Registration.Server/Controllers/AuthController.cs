683f6e7e64e50eea22a7fe4c8c8c8b0bea2926bb
churlohx <churl.oh@perkinelmer.com>
2017-01-13 15:38:32 -0500

    Improve the error logging for login process

--- subprojects/asp.net/Registration/PresentationTier/Registration.Server/Controllers/AuthController.cs
+++ subprojects/asp.net/Registration/PresentationTier/Registration.Server/Controllers/AuthController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Web.Http;
 using System.Xml;
 
@@ -17,31 +18,43 @@ namespace PerkinElmer.COE.Registration.Server.Controllers
         [Route("api/auth/login")]
         public HttpResponseMessage Login()
         {
-            var userData = Request.Content.ReadAsAsync<JObject>().Result;
-            var webClient = new WebClient();
-            var loginUrl = string.Format("/COESingleSignOn/SingleSignOn.asmx/GetAuthenticationTicket?userName={0}&password={1}", userData["username"], userData["password"]);
-            loginUrl = GetAbsoluteUrl(loginUrl, true);
-            string token;
-            using (var loginResponse = new StreamReader(webClient.OpenRead(loginUrl)))
-                token = loginResponse.ReadToEnd();
-            var xml = new XmlDocument();
-            xml.LoadXml(token);
-            token = xml.DocumentElement.FirstChild.InnerText.Trim();
-            var response = Request.CreateResponse(HttpStatusCode.OK, new JObject(
-                new JProperty("data", new JObject(new JProperty("msg", "LOGIN SUCCESSFUL"))),
-                new JProperty("meta",
-                    new JObject(
-                        new JProperty("token", token),
-                        new JProperty("profile", new JObject(new JProperty("fullName", userData["username"])))
+            var errorMessage = new StringBuilder();
+            try
+            {
+                var userData = Request.Content.ReadAsAsync<JObject>().Result;
+                errorMessage.AppendLine(string.Format("Logging in user, {0}...", userData["username"]));
+                var webClient = new WebClient();
+                var loginUrl = string.Format("/COESingleSignOn/SingleSignOn.asmx/GetAuthenticationTicket?userName={0}&password=", userData["username"]);
+                loginUrl = GetAbsoluteUrl(loginUrl, true);
+                string token;
+                errorMessage.AppendLine(string.Format("Opening {0}...", loginUrl));
+                using (var loginResponse = new StreamReader(webClient.OpenRead(loginUrl + userData["password"])))
+                    token = loginResponse.ReadToEnd();
+                var xml = new XmlDocument();
+                xml.LoadXml(token);
+                token = xml.DocumentElement.FirstChild.InnerText.Trim();
+                errorMessage.AppendLine(string.Format("Token: {0}", token));
+                var response = Request.CreateResponse(HttpStatusCode.OK, new JObject(
+                    new JProperty("data", new JObject(new JProperty("msg", "LOGIN SUCCESSFUL"))),
+                    new JProperty("meta",
+                        new JObject(
+                            new JProperty("token", token),
+                            new JProperty("profile", new JObject(new JProperty("fullName", userData["username"])))
+                        )
                     )
-                )
-            ));
-            var cookie = new CookieHeaderValue("COESSO", token);
-            cookie.Expires = DateTime.Now.AddMinutes(60);
-            cookie.Domain = Request.RequestUri.Host;
-            cookie.Path = "/";
-            response.Headers.AddCookies(new CookieHeaderValue[] { cookie });
-            return response;
+                ));
+                var cookie = new CookieHeaderValue("COESSO", token);
+                cookie.Expires = DateTime.Now.AddMinutes(60);
+                cookie.Domain = Request.RequestUri.Host;
+                cookie.Path = "/";
+                response.Headers.AddCookies(new CookieHeaderValue[] { cookie });
+                return response;
+            }
+            catch (Exception ex)
+            {
+                errorMessage.AppendLine("Login failed!");
+                throw new InvalidOperationException(errorMessage.ToString(), ex);
+            }
         }
     }
 }
