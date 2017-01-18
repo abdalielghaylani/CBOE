f3d12bc475ebd47e0680b1423726d300c7aac101
Churl Oh <churl.oh@perkinelmer.com>
2017-01-18 12:03:32 -0500

    Improve unit testing

--- subprojects/registration.client2/src/store/configuration/configuration.reducer.test.ts
+++ subprojects/registration.client2/src/store/configuration/configuration.reducer.test.ts
@@ -16,11 +16,30 @@ describe('configuration reducer', () => {
   });
 
   it('should update table and clear rows on OPEN_TABLE', () => {
-    const previousValue = initState.tableId;
+    const data = 'table';
+    const previousState = initState;
     const nextState = configurationReducer(
-      initState,
-      { type: ConfigurationActions.OPEN_TABLE, payload: { tableId: 'table' } });
-    expect(nextState.tableId).toEqual('table');
+      previousState,
+      ConfigurationActions.openTableAction(data));
+    expect(nextState.tableId).toEqual(data);
     expect(nextState.rows).toEqual([]);
+  });
+
+  it('should update rows on OPEN_TABLE_SUCCESS', () => {
+    const data = [{ c1: 'v11', c2: 'v12' }, { c1: 'v21', c2: 'v22' }];
+    const previousState = initState;
+    const nextState = configurationReducer(
+      previousState,
+      ConfigurationActions.openTableSuccessAction(data));
+    expect(nextState.rows).toEqual(data);
+  });
+
+  it('should update lookups on LOAD_LOOKUPS_SUCCESS', () => {
+    const data = { users: [{ id: 1, name: 'user one' }] };
+    const previousState = initState;
+    const nextState = configurationReducer(
+      previousState,
+      ConfigurationActions.loadLookupsSuccessAction(data));
+    expect(nextState.lookups).toEqual(data);
   });
 });
--- subprojects/registration.client2/src/store/registry/registry.reducer.test.ts
+++ subprojects/registration.client2/src/store/registry/registry.reducer.test.ts
@@ -1,7 +1,7 @@
 import { Iterable } from 'immutable';
 import { RegistryFactory } from './registry.initial-state';
 import { registryReducer } from './registry.reducer';
-import { RegistryActions, SessionActions } from '../../actions';
+import { RegistryActions, RecordDetailActions } from '../../actions';
 import { IRegistryRecord } from './registry.types';
 
 describe('registry reducer', () => {
@@ -23,22 +23,64 @@ describe('registry reducer', () => {
     expect(nextState).toEqual(initState);
   });
 
-  it('should send data on OPEN_RECORDS_SUCCESS', () => {
-    let data = [{ c1: 'v11', c2: 'v12' }, { c1: 'v21', c2: 'v22' }];
-    const previousValue = initState.temporary;
-    const nextState = registryReducer(
-      initState,
-      RegistryActions.openRecordsSuccessAction(true, data)
-    );
-    expect(nextState.tempRecords.rows).toEqual(data);
-  });
-
   it('should clear rows on OPEN_RECORDS', () => {
     const previousValue = initState.temporary;
-    const nextState = registryReducer(
+    const firstState = registryReducer(
       initState,
       RegistryActions.openRecordsAction(true)
     );
-    expect(nextState.tempRecords.rows).toEqual([]);
+    expect(firstState.tempRecords.rows).toEqual([]);
+    const secondState = registryReducer(
+      firstState,
+      RegistryActions.openRecordsAction(false)
+    );
+    expect(secondState.records.rows).toEqual([]);
+  });
+
+  it('should send data on OPEN_RECORDS_SUCCESS', () => {
+    const data = [{ c1: 'v11', c2: 'v12' }, { c1: 'v21', c2: 'v22' }];
+    const firstState = registryReducer(
+      initState,
+      RegistryActions.openRecordsSuccessAction(true, data)
+    );
+    expect(firstState.tempRecords.rows).toEqual(data);
+    const data2 = data.concat({ c1: 'v31', c2: 'v32' });
+    const secondState = registryReducer(
+      firstState,
+      RegistryActions.openRecordsSuccessAction(false, data2)
+    );
+    expect(secondState.tempRecords.rows).toEqual(data);
+    expect(secondState.records.rows).toEqual(data2);
+  });
+
+  it('should set record data on RETRIEVE_RECORD_SUCCESS', () => {
+    const id = 100;
+    const data = '<xml>encoded-temp-cdxml-data</xml>';
+    const firstState = registryReducer(
+      initState,
+      RegistryActions.retrieveRecordSuccessAction(true, id, data)
+    );
+    expect(firstState.temporary).toEqual(true);
+    expect(firstState.currentId).toEqual(id);
+    expect(firstState.data).toEqual(data);
+    const id2 = 101;
+    const data2 = '<xml>encoded-cdxml-data</xml>';
+    const secondState = registryReducer(
+      firstState,
+      RegistryActions.retrieveRecordSuccessAction(false, id2, data2)
+    );
+    expect(secondState.temporary).toEqual(false);
+    expect(secondState.currentId).toEqual(id2);
+    expect(secondState.data).toEqual(data2);
+  });
+
+  it('should set structureData on LOAD_STRUCTURE_SUCCESS', () => {
+    const data = '<CDXML/>';
+    const payload = { data: data };
+    const nextState = registryReducer(
+      initState,
+      RecordDetailActions.loadStructureSuccessAction(payload)
+    );
+    expect(nextState.structureData).toEqual(data);
   });
 });
