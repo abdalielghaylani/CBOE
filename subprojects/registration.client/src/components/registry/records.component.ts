501e23aed4cdc174097766be163feef5abeda1a9
sreejithsdev <sreejith.sahadevan@perkinelmer.com>
2017-02-23 17:30:20 +0530

    CBOE Registration client :- CBOE-4734 Create option to save query list,print records in permanant registry search,Fix UI defects (#14)
    * CBOE Registration Client :-  Changes to Add Full screen option.
    
    * CBOE Registration Client :-  Fixed full screen issues
    
    * CBOE Registration client :- Full screen style changes
    
    * CBOE Registration :- Advanced Search option in Permanant Registry - initial commit
    
    * CBOE REG Client-:  CBOE-4675  Create View model for Advanced Registry search form
    
    * CBOE-4676 :- Create user interface and view model for "Query Management" (recent and saved queries)
    
    * CBOE-4674 - Lookup in Registry search form should be filled with configured data
    CBOE-4673 - Create action and reducers for "Permanent registry search"
    
    * Fixed lint error
    
    * CBOE Registration client :- Fixed issue in registry search actions
    
    * CBOE Registration Client:-Unit test issue fixed.
    
    * CBOE Registration client :- UI changes, CBOE-4702 :-Create view,model and actions for 'Preference setting'
    
    * CBOE Registration client :- CBOE-4734 Create option to save query list,print records in permanant registry search,Fix UI defects

--- subprojects/registration.client/src/components/registry/records.component.ts
+++ subprojects/registration.client/src/components/registry/records.component.ts
@@ -14,15 +14,32 @@ import { Subscription } from 'rxjs/Subscription';
 import { RegistryActions } from '../../actions';
 import { IAppState, IRecords } from '../../store';
 import { DxDataGridComponent } from 'devextreme-angular';
+import * as regSearchTypes from './registry-search.types';
 
 @Component({
   selector: 'reg-records',
   template: `
-    <div class="container-fluid border-light background-white pb2">
+  <div class="container-fluid border-light background-white pb2">
       <reg-page-header testid="configuration-heading" id="qa-configuration-heading">
             <span *ngIf="records.temporary">Temporary</span> Registration Records
       </reg-page-header>
-      <dx-data-grid [columns]=records.gridColumns [dataSource]=records.rows [paging]='{pageSize: 10}' 
+      <dx-popup
+        [width]="300"
+        [height]="250"
+        [showTitle]="true"
+        title="Save Query"
+        [dragEnabled]="false"
+        [closeOnOutsideClick]="true"
+        [(visible)]="popupVisible">
+        <div *dxTemplate="let data of 'content'">
+         <dx-form id="editHitlistForm" labelLocation="left" minColWidth=300 [colCount]="1" [items]="regsearch.editColumns"></dx-form>
+          <div style="margin-top: 15px">
+          <button class="mr1 btn btn-primary">Save</button>
+          <button class="mr1 btn btn-primary" (click)="cancelSaveQuery()">Cancel</button>
+          </div>
+        </div>
+      </dx-popup>
+      <dx-data-grid id="grdRecords" [columns]=records.gridColumns [dataSource]=records.rows [paging]='{pageSize: 10}' 
         [pager]='{ showPageSizeSelector: true, allowedPageSizes: [5, 10, 20], showInfo: true }'
         [searchPanel]='{ visible: true }' [filterRow]='{ visible: true }' rowAlternationEnabled=true
         (onToolbarPreparing)="onToolbarPreparing($event)"
@@ -32,9 +49,24 @@ import { DxDataGridComponent } from 'devextreme-angular';
         (onEditingStart)='onEditingStart($event)'
         (onRowRemoving)='onRowRemoving($event)'>
         <dxo-editing mode="row" [allowUpdating]="true" [allowDeleting]="records.temporary" [allowAdding]="false"></dxo-editing>
+        <dxo-selection mode="multiple"></dxo-selection>
+        <div *dxTemplate="let data of 'content'">
+        <div class="btn-group btn-group-sm">
+          <button class="btn btn-group btn-group-sm rose text-relaxed"  data-original-title="New Query" (click)="newQuery()">
+          <i class="fa fa-list-alt"></i>New Query</button>
+        </div>
+        <div class="btn-group btn-group-sm">
+          <button class="btn btn-group btn-group-sm rose text-relaxed" (click)="saveQuery()" data-original-title="Save Query">
+          <i class="fa fa-floppy-o"></i>Save Query</button>
+        </div>
+        <div class="btn-group btn-group-sm">
+          <button class="btn btn-group btn-group-sm rose text-relaxed"  data-original-title="Print" (click)="printRecords()"  >
+          <i class="fa fa-print"></i>Print</button>
+        </div>
+       </div>
       </dx-data-grid>
-    </div>
-  `,
+    </div>     
+     `,
   styles: [require('./records.css')],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -47,12 +79,14 @@ export class RegRecords implements OnInit, OnDestroy {
   private recordsSubscription: Subscription;
   private lookups: any;
   private records: IRecords;
+  private popupVisible: boolean = false;
+  private regsearch: regSearchTypes.CSaveQuery = new regSearchTypes.CSaveQuery();
 
   constructor(
     private router: Router,
     private ngRedux: NgRedux<IAppState>,
     private registryActions: RegistryActions,
-    private changeDetector: ChangeDetectorRef) { 
+    private changeDetector: ChangeDetectorRef) {
     this.records = { temporary: this.temporary, rows: [], gridColumns: [] };
   }
 
@@ -120,13 +154,8 @@ export class RegRecords implements OnInit, OnDestroy {
   onToolbarPreparing(e) {
     if (!this.records.temporary) {
       e.toolbarOptions.items.unshift({
-        location: 'after',
-        widget: 'dxButton',
-        options: {
-          icon: 'find',
-          text: 'Advanced Search',
-          onClick: this.onSearchClick.bind(this)
-        }
+        location: 'before',
+        template: 'content'
       });
     }
   }
@@ -149,4 +178,48 @@ export class RegRecords implements OnInit, OnDestroy {
 
   onRowRemoving(e) {
   }
+
+  newQuery() {
+    this.router.navigate(['records/search']);
+  }
+
+  saveQuery() {
+    this.popupVisible = true;
+  }
+
+  cancelSaveQuery() {
+    this.popupVisible = false;
+  }
+
+  printRecords() {
+    let printContents, popupWin;
+    printContents = document.getElementById('grdRecords').innerHTML;
+    popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
+    popupWin.document.open();
+    popupWin.document.write(`<html>
+        <head>
+          <title>Print table</title>
+          <style>
+          table, th, td 
+          {
+              border:solid 1px #f0f0f0;
+              font:15px Verdana;
+          }
+          .dx-header-row {
+              font-weight:bold;
+          }
+          img {
+            height:120px;
+          }
+          .dx-datagrid-header-panel,.dx-datagrid-filter-row,.dx-datagrid-pager,
+          .dx-command-edit,.dx-command-select{
+            display:none;
+          }
+          </style>
+        </head>
+    <body onload="window.print();window.close()">${printContents}</body>
+      </html>`);
+    popupWin.document.close();
+  }
+
 };
