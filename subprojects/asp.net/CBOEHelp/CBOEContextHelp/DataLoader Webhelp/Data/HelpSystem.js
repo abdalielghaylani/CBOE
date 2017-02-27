b47b2070ffe3487aa04b28940e4a91e4a2598166
churlohx <churl.oh@perkinelmer.com>
2017-02-27 12:44:52 -0500

    Remove CBOEHelp project from dependency Partial fix for CBOE-4750: Support CBOE 12.6.3 version

--- subprojects/asp.net/CBOEHelp/CBOEContextHelp/DataLoader Webhelp/Data/HelpSystem.js
+++
@@ -1,7 +0,0 @@
-CMCXmlParser._FilePathToXmlStringMap.Add(
-	'HelpSystem',
-	'<?xml version=\"1.0\" encoding=\"utf-8\"?>' +
-	'<WebHelpSystem DefaultUrl=\"Content/DataLoader/Data Loader Introduction.htm\" Toc=\"Data/Toc.xml\" Index=\"Data/Index.xml\" Concepts=\"Data/Concepts.xml\" Glossary=\"Content/Glossary.htm\" SearchDatabase=\"Data/Search.xml\" Alias=\"Data/Alias.xml\" Synonyms=\"Data/Synonyms.xml\" Skin=\"Data/SkinDataLoader Skin/Skin.xml\" Skins=\"DataLoader Skin,Dataview Manager Skin,Default\" BuildTime=\"9/12/2013 3:06:49 PM\" BuildVersion=\"8.1.2.0\" TargetType=\"WebHelp\" SkinTemplateFolder=\"Skin/\" InPreviewMode=\"false\" MoveOutputContentToRoot=\"false\" MakeFileLowerCase=\"false\" UseCustomTopicFileExtension=\"false\">' +
-	'    <!-- saved from url=(0014)about:internet -->' +
-	'</WebHelpSystem>'
-);
--- subprojects/asp.net/CBOEHelp/CBOEContextHelp/Dataview Manager Webhelp/Data/Alias.js
+++
@@ -1,7 +0,0 @@
-CMCXmlParser._FilePathToXmlStringMap.Add(
-	'Alias',
-	'<?xml version=\"1.0\" encoding=\"utf-8\"?>' +
-	'<CatapultAliasFile DefaultSkinName=\"Dataview Manager Skin\">' +
-	'    <!-- saved from url=(0016)http://localhost -->' +
-	'</CatapultAliasFile>'
-);
