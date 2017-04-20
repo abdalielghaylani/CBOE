56cb6bf2a3f398dcaf9e2bfa20f6e11073e0dfc2
churlohx <churl.oh@perkinelmer.com>
2017-04-20 14:37:56 -0400

    Support sorting of temporary records

--- subprojects/asp.net/Registration/PresentationTier/Registration.Server/Controllers/RegistryRecordsController.cs
+++ subprojects/asp.net/Registration/PresentationTier/Registration.Server/Controllers/RegistryRecordsController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -9,21 +11,52 @@ namespace PerkinElmer.COE.Registration.Server.Controllers
 {
     public class RegistryRecordsController : RegControllerBase
     {
+        private class DataColumn
+        {
+            public string definition;
+            public string label;
+            public bool sortable;
+        }
+
+        private static string CleanupSortTerm(DataColumn[] columns, string sortTerm)
+        {
+            const string sortDesc = " desc";
+            const string sortAsc = " asc";
+            var desc = sortTerm.EndsWith(sortDesc);
+            var t = sortTerm.Replace(sortDesc, string.Empty).Replace(sortAsc, string.Empty).Trim();
+            var dc = columns.FirstOrDefault(c => c.definition.Equals(t));
+            if (dc == null) dc = columns.FirstOrDefault(c => c.label != null && c.label.Equals(t));
+            t = dc == null ? string.Empty : dc.definition;
+            if (desc) t += sortDesc;
+            return t;
+        }
+
         #region Permanent Records
         [Route("api/records")]
         public JObject Get(int? skip = null, int? count = null, string sort = null)
         {
-            var records = new JArray();
+            CheckAuthentication();
+            var columns = new DataColumn[]
+            {
+                new DataColumn{ definition = "regid", label = "id", sortable = true },
+                new DataColumn{ definition = "name", sortable = true },
+                new DataColumn{ definition = "created", sortable = true },
+                new DataColumn{ definition = "modified", sortable = true },
+                new DataColumn{ definition = "personcreated", label ="creator", sortable = true },
+                new DataColumn{ definition = "'record/' || regid || '?' || to_char(modified, 'YYYYMMDDHH24MISS')", label = "structure", sortable = false },
+                new DataColumn{ definition = "regnumber", sortable = true },
+                new DataColumn{ definition = "statusid", label = "status", sortable = true },
+                new DataColumn{ definition = "approved", sortable = true }
+            };
+            var selectTerms = String.Join(", ", columns.Select(c => c.definition + (c.label != null ? " " + c.label : string.Empty)));
             // Default sorting order is descending order by modified date
-            if (string.IsNullOrEmpty(sort)) sort = "modified DESC";
+            if (string.IsNullOrEmpty(sort)) sort = string.Empty;
+            var sortTerms = String.Join(", ", sort.ToLower().Split(new char[] { ',' }).Select(t => t.Trim())
+                .Select(t => CleanupSortTerm(columns, t)).Where(t => !string.IsNullOrEmpty(t)));
+            if (string.IsNullOrEmpty(sortTerms)) sortTerms = "modified desc";
             // Make the sorting unique
-            if (!sort.ToLower().Contains("regid")) sort += ", regid";
-            string query = string.Format(
-                "SELECT regid id, name, created, modified, personcreated as creator, " +
-                    "'record/' || regid || '?' || to_char(modified, 'YYYYMMDDHH24MISS') as structure, " +
-                    "regnumber, statusid as status, approved " +
-                "FROM vw_mixture_regnumber a " +
-                "ORDER BY {0}", sort);
+            if (!sortTerms.Contains("regid")) sort += ", regid";
+            string query = string.Format("SELECT {0} FROM vw_mixture_regnumber a ORDER BY {1}", selectTerms, sortTerms);
             return new JObject(
                 new JProperty("temporary", false),
                 new JProperty("rows", ExtractData(query, null, skip, count)),
@@ -44,7 +77,26 @@ namespace PerkinElmer.COE.Registration.Server.Controllers
         public JObject GetTemp(int? skip = null, int? count = null, string sort = null)
         {
             CheckAuthentication();
-            var query = "SELECT tempcompoundid id, tempbatchid batchid, formulaweight MW, molecularformula MF, datecreated created, datelastmodified modified, personcreated as creator, 'temprecord/' || tempcompoundid || '?' || to_char(datelastmodified, 'YYYYMMDDHH24MISS') as structure FROM vw_temporarycompound ORDER BY tempbatchid DESC";
+            var columns = new DataColumn[]
+            {
+                new DataColumn{ definition = "tempcompoundid", label = "id", sortable = true },
+                new DataColumn{ definition = "tempbatchid", label = "batchid", sortable = true },
+                new DataColumn{ definition = "formulaweight", label = "mw", sortable = true },
+                new DataColumn{ definition = "molecularformula", label = "mf", sortable = true },
+                new DataColumn{ definition = "datecreated", label = "created", sortable = true },
+                new DataColumn{ definition = "datelastmodified", label = "modified", sortable = true },
+                new DataColumn{ definition = "personcreated", label ="creator", sortable = true },
+                new DataColumn{ definition = "'temprecord/' || tempcompoundid || '?' || to_char(datelastmodified, 'YYYYMMDDHH24MISS')", label = "structure", sortable = false }
+            };
+            var selectTerms = String.Join(", ", columns.Select(c => c.definition + (c.label != null ? " " + c.label : string.Empty)));
+            // Default sorting order is descending order by modified date
+            if (string.IsNullOrEmpty(sort)) sort = string.Empty;
+            var sortTerms = String.Join(", ", sort.ToLower().Split(new char[] { ',' }).Select(t => t.Trim())
+                .Select(t => CleanupSortTerm(columns, t)).Where(t => !string.IsNullOrEmpty(t)));
+            if (string.IsNullOrEmpty(sortTerms)) sortTerms = "datelastmodified desc";
+            // Make the sorting unique
+            if (!sortTerms.Contains("tempcompoundid")) sort += ", tempcompoundid";
+            string query = string.Format("SELECT {0} FROM vw_temporarycompound a ORDER BY {1}", selectTerms, sortTerms);
             return new JObject(
                 new JProperty("temporary", true),
                 new JProperty("rows", ExtractData(query, null, skip, count)),
