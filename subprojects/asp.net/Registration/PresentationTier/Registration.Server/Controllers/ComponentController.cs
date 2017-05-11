8b1992e6f07ff38621fb1834d2ed446df8820c91
raviprasad-jogi <raviprasad.jogi@perkinelmer.com>
2017-05-11 11:35:21 -0400

    CBOE-5027  Create Web API - CRUD Operations on Components III (#44)
    Done changes required in model and the update functionality has been implemented for component section and developer testing is done.

--- subprojects/asp.net/Registration/PresentationTier/Registration.Server/Controllers/ComponentController.cs
+++ subprojects/asp.net/Registration/PresentationTier/Registration.Server/Controllers/ComponentController.cs
@@ -9,12 +9,27 @@ using CambridgeSoft.COE.Registration.Services;
 using Swashbuckle.Swagger.Annotations;
 using PerkinElmer.COE.Registration.Server.Code;
 using Microsoft.Web.Http;
+using CambridgeSoft.COE.Registration.Services.Types;
+using CambridgeSoft.COE.Registration.Access;
+using System.Net;
+using System.Xml;
 
 namespace PerkinElmer.COE.Registration.Server.Controllers
 {
     [ApiVersion(Consts.apiVersion)]
     public class ComponentController : RegControllerBase
     {
+        private RegistrationOracleDAL regDal = null;
+
+        private RegistrationOracleDAL RegDal
+        {
+            get
+            {
+                if (regDal == null)
+                    DalUtils.GetRegistrationDAL(ref regDal, CambridgeSoft.COE.Registration.Constants.SERVICENAME);
+                return regDal;
+            }
+        }
         /// <summary>
         /// Returns the list of all components
         /// </summary>
@@ -27,24 +42,35 @@ namespace PerkinElmer.COE.Registration.Server.Controllers
         [Route(Consts.apiPrefix + "components")]
         [SwaggerOperation("GetComponents")]
         [SwaggerResponse(200, type: typeof(List<JObject>))]
-        public async Task<IHttpActionResult> GetComponents(int? recordId = null, int? skip = null, int? count = null, string sort = null)
+        public async Task<IHttpActionResult> GetComponents(string recordId, int? skip = null, int? count = null, string sort = null)
         {
             CheckAuthentication();
-            // Get the list of components asynchronously
-            // And return the list
-            List<JObject> data = null;
-            return Ok(data);
+            var tableName = "REGDB.VW_MIXTURE_STRUCTURE";
+            var whereClause = " WHERE REGNUMBER=:regNumber";
+            var query = GetQuery(tableName + whereClause, ComponentRecordColumns, sort, "COMPONENTID", "STRUCTUREID");
+            var args = new Dictionary<string, object>();
+            args.Add(":regNumber", recordId);
+            var responseMessage = Request.CreateResponse(HttpStatusCode.OK, new JObject(
+                new JProperty("VW_MIXTURE_STRUCTURE", false),
+                new JProperty("regNumber", recordId),
+                new JProperty("totalCount", Convert.ToInt32(ExtractValue("SELECT cast(count(1) as int) c FROM " + tableName + whereClause, args))),
+                new JProperty("startIndex", skip == null ? 0 : Math.Max(skip.Value, 0)),
+                new JProperty("rows", ExtractData(query, args, skip, count))
+            ));
+            return await Task.FromResult<IHttpActionResult>(ResponseMessage(responseMessage));
         }
 
         [HttpGet]
         [Route(Consts.apiPrefix + "components/{id}")]
         [SwaggerOperation("GetComponent")]
         [SwaggerResponse(200, type: typeof(JObject))]
-        public async Task<IHttpActionResult> GetComponent(int id)
-        {
+        public async Task<IHttpActionResult> GetComponent(string id, string regnumber)
+        {            
             CheckAuthentication();
-            if (id < 0) return NotFound();
-            return Ok(new JObject());
+            JArray data = new JArray();           
+            data = ExtractData(" SELECT STRUCTUREID, COMPONENTID, STRUCT_NAME, STRUCT_COMMENTS, CMP_COMMENTS, MOLECULARFORMULA, FORMULAWEIGHT, NORMALIZEDSTRUCTURE FROM REGDB.VW_MIXTURE_STRUCTURE WHERE REGNUMBER = '" + regnumber + "' AND COMPONENTID = '" + id + "'");
+            var responseMessage = Request.CreateResponse(HttpStatusCode.OK, data);
+            return await Task.FromResult<IHttpActionResult>(ResponseMessage(responseMessage));
         }
 
         [HttpPost]
@@ -53,7 +79,7 @@ namespace PerkinElmer.COE.Registration.Server.Controllers
         [SwaggerResponse(201, type: typeof(JObject))]
         public async Task<IHttpActionResult> CreateComponent(JObject component)
         {
-            CheckAuthentication();
+            CheckAuthentication();            
             var id = -1;
             return Created<JObject>(string.Format("{0}components/{1}", Consts.apiPrefix, id), component);
         }
@@ -62,10 +88,15 @@ namespace PerkinElmer.COE.Registration.Server.Controllers
         [Route(Consts.apiPrefix + "components/{id}")]
         [SwaggerOperation("UpdateComponent")]
         [SwaggerResponse(200, type: typeof(string))]
-        public async Task<IHttpActionResult> UpdateComponent(int id)
+        public async Task<IHttpActionResult> UpdateComponent(JObject data)
         {
+            string _dalResponseXml = "";
             CheckAuthentication();
-            return Ok(string.Format("The component #{0} was updated successfully!", id));
+            var jData = Request.Content.ReadAsAsync<JObject>().Result;
+            RegistrationOracleDAL registrationoracleDAL = new RegistrationOracleDAL();
+            XmlDocument datatoXml = Newtonsoft.Json.JsonConvert.DeserializeXmlNode(data.ToString());
+            RegDal.UpdateRegistryRecord(datatoXml.InnerXml, CambridgeSoft.COE.Registration.DuplicateCheck.CompoundCheck, out _dalResponseXml);
+            return Ok(string.Format("The component #{0} was updated successfully!", _dalResponseXml));
         }
 
         [HttpDelete]
@@ -73,9 +104,13 @@ namespace PerkinElmer.COE.Registration.Server.Controllers
         [SwaggerOperation("DeleteComponent")]
         [SwaggerResponse(200, type: typeof(string))]
         public async Task<IHttpActionResult> DeleteComponent(int id)
-        {
+        {            
             CheckAuthentication();
-            return Ok(string.Format("The component #{0} was deleted successfully!", id));
+            RegistryRecord registryRecord = null;
+            registryRecord.DeleteComponent(id);
+            var responseMessage = Request.CreateResponse(HttpStatusCode.OK, string.Format("The component #{0} was deleted successfully!", id));
+            return await Task.FromResult<IHttpActionResult>(ResponseMessage(responseMessage));           
         }
+        
     }
 }
