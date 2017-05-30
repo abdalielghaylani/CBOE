d48ca9bd566a89eb8010bb9f7f847af7dcfe445c
churlohx <churl.oh@perkinelmer.com>
2017-05-30 11:18:44 -0400

    Add a simple data model

---
+++ subprojects/asp.net/Registration/PresentationTier/Registration.Server/Models/SimpleData.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace PerkinElmer.COE.Registration.Server.Models
+{
+    /// <summary>
+    /// The class for the simple data object
+    /// </summary>
+    public partial class SimpleData
+    {
+        [JsonConstructor]
+        public SimpleData(string data)
+        {
+            Data = data;
+        }
+
+        /// <summary>
+        /// Gets or sets the data
+        /// </summary>
+        [JsonProperty(PropertyName = "data")]
+        public string Data { get; set; }
+    }
+}
