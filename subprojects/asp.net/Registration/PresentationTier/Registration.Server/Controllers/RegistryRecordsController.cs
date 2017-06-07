7887a50bca5fd3a3d58d75b4e9111b6546517afb
COEDEV3\Administrator <vijay.krishnan@perkinelmer.com>
2017-06-07 04:29:23 -0400

    CBOE-5254  - Provide the duplicate exception in API

--- subprojects/asp.net/Registration/PresentationTier/Registration.Server/Controllers/RegistryRecordsController.cs
+++ subprojects/asp.net/Registration/PresentationTier/Registration.Server/Controllers/RegistryRecordsController.cs
@@ -422,5 +422,29 @@ namespace PerkinElmer.COE.Registration.Server.Controllers
             });
         }
         #endregion
+
+        #region Check Duplicate
+
+        [HttpPost]
+        [Route(Consts.apiPrefix + "checkDuplicate")]
+        [SwaggerOperation("CheckDuplicate")]
+        [SwaggerResponse(201, type: typeof(ResponseData))]
+        [SwaggerResponse(400, type: typeof(JObject))]
+        [SwaggerResponse(401, type: typeof(JObject))]
+        public async Task<IHttpActionResult> CheckDuplicate(CheckDuplicateData data)
+        {
+            return await CallServiceMethod((service) =>
+            {
+                string duplicateCheck = data.DuplicateCheck;
+                var doc = new XmlDocument();
+                doc.LoadXml(data.DataXML);
+                var recordString = ChemistryHelper.ConvertStructuresToCdx(doc).OuterXml;
+                var resultString = service.CheckUniqueRegistryRecord(recordString, duplicateCheck);
+                if (!string.IsNullOrEmpty(resultString))
+                    throw new RegistrationException(resultString);
+                return new ResponseData(null, null, resultString, null);
+            });
+        }
+        #endregion
     }
 }
---
+++ subprojects/asp.net/Registration/PresentationTier/Registration.Server/Models/CheckDuplicateData.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+namespace PerkinElmer.COE.Registration.Server.Models
+{
+    /// <summary>
+    /// The class for the CheckDuplicate data object
+    /// </summary>
+    public class CheckDuplicateData
+    {
+        [JsonConstructor]
+        public CheckDuplicateData(string dataXML, string duplicateCheck)
+        {
+            DataXML = dataXML;
+            DuplicateCheck = duplicateCheck;
+        }
+
+        /// <summary>
+        /// Gets or sets the dataXML
+        /// </summary>
+        [JsonProperty(PropertyName = "dataXML")]
+        public string DataXML { get; set; }
+
+        /// <summary>
+        /// Gets or sets the duplicateCheck
+        /// </summary>
+        [JsonProperty(PropertyName = "duplicateCheck")]
+        public string DuplicateCheck { get; set; }
+    }
+}
