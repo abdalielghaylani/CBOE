2115e70fbf0b90b0f22c33c4c168c9fea7d58322
Churl Oh <churl.oh@perkinelmer.com>
2017-06-13 15:31:43 -0400

    Add a utility to process custom table configuration

---
+++ subprojects/registration.client/src/common/utils/view.utils.ts
@@ -0,0 +1,29 @@
+export interface IColumnConfig {
+  name: string;
+  label?: string;
+  type?: string;
+  idField?: boolean;
+  lookup?: any[];
+};
+
+/**
+ * Returns the view compatible column list from the column configuration list from the server
+ * @param columnConfig The list of column configuration data from server
+ */
+export function getViewColumns(columnConfig: IColumnConfig[]): any[] {
+  let viewColumns = [];
+  columnConfig.forEach(cc => {
+    let viewColumn: any = {};
+    viewColumn.dataField = cc.name;
+    viewColumn.caption = cc.label ? cc.label : cc.name;
+    if (cc.lookup && cc.lookup.length > 0) {
+      let keys = Object.keys(cc.lookup[0]);
+      viewColumn.lookup = {
+        dataSource: cc.lookup,
+        valueExpr: keys[0],
+        displayExpr: keys[1]
+      };
+    }
+  });
+  return viewColumns;
+}
