9689a3b25c606f66d762f2c59d99d37d05515dad
churlohx <churl.oh@perkinelmer.com>
2017-06-15 14:28:22 -0400

    Use a form to edit hit-list entries

--- subprojects/registration.client/src/components/registry/registry-search.types.ts
+++ subprojects/registration.client/src/components/registry/registry-search.types.ts
@@ -205,10 +205,12 @@ export class CSearchFormVM {
 export const HITLIST_GRID_COLUMNS = [{
   dataField: 'name',
   dataType: 'String',
-  cellTemplate: 'saveCellTemplate'
+  cellTemplate: 'saveCellTemplate',
+  formItem: { colSpan: 2 }
 }, {
   dataField: 'description',
-  dataType: 'String'
+  dataType: 'String',
+  formItem: { colSpan: 2 }
 }, {
   dataField: 'isPublic',
   width: '60px'
@@ -217,24 +219,29 @@ export const HITLIST_GRID_COLUMNS = [{
   dataField: 'numberOfHits',
   dataType: Number,
   allowEditing: false,
-  width: '60px'
+  width: '60px',
+  formItem: { visible: false }
 }, {
   dataField: 'dateCreated',
   caption: 'Date Created',
   dataType: 'date',
   format: 'shortDateShortTime',
-  allowEditing: false
+  allowEditing: false,
+  formItem: { visible: false }
 }, {
   dataField: 'hitlistType',
   caption: 'Queries',
   lookup: { dataSource: [{ id: 'TEMP', name: 'Recent' }, { id: 'SAVED', name: 'Saved' }], valueExpr: 'id', displayExpr: 'name' },
   groupIndex: 0,
-  allowEditing: false
+  allowEditing: false,
+  formItem: { visible: false }
 },
 {
-  caption: 'Restore',
+  caption: 'Commands',
   cellTemplate: 'restoreCellTemplate',
-  width: '200px'
+  width: '200px',
+  allowEditing: false,
+  formItem: { visible: false }
 }
 ];
 
