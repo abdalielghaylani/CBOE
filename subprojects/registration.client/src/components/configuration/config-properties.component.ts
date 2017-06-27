6b8b8bd0a757b13a6faaf938920976825c20b0dc
SHIJU <shijumon.kulangara@perkinelmer.com>
2017-06-27 10:46:37 -0400

    Fix error message not appearing in the client when new property is added/updated (#115)

--- subprojects/registration.client/src/components/configuration/config-properties.component.ts
+++ subprojects/registration.client/src/components/configuration/config-properties.component.ts
@@ -109,14 +109,18 @@ export class RegConfigProperties implements OnInit, OnDestroy {
   addProperty(e) {
     this.dataSource.insert(this.configProperties.formData).then((result) => {
       this.grid.instance.refresh();
-    });
+    })
+      .fail((err) => notifyError(err.message)
+      );
     this.cancel();
   }
 
   saveProperty(e) {
     this.dataSource.update(this.configProperties.formData, []).then((result) => {
       this.grid.instance.refresh();
-    });
+    })
+      .fail((err) => notifyError(err.message)
+      );
     this.cancel();
   }
 
