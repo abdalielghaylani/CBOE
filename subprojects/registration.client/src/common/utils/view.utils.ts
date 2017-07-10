e8bdd90c815a204e8775945919d8a0c771910a0a
shijumon <shijumon.kulangara@perkinelmer.com>
2017-07-10 13:20:01 -0400

    Set edit button visibility in details view based on user privilges

--- subprojects/registration.client/src/common/utils/view.utils.ts
+++ subprojects/registration.client/src/common/utils/view.utils.ts
@@ -27,13 +27,3 @@ export function getViewColumns(columnConfig: IColumnConfig[]): any[] {
   });
   return viewColumns;
 }
-
-export function isUserHasPrivilege(privilege: string, userPrivileges: any[]): boolean {
-  if (userPrivileges) {
-    let privilageItem = userPrivileges.find(p => p.name === privilege);
-    if (privilageItem !== undefined) {
-      return true;
-    }
-  }
-  return false;
-}
