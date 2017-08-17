9147bbb611c305c5100aa623b5084184699e701f
Churl Oh <churl.oh@perkinelmer.com>
2017-08-16 20:08:56 -0400

    Don’t keep adding project list

--- subprojects/registration.client/src/components/registry/base/projects-form-item/projects-form-item.component.ts
+++ subprojects/registration.client/src/components/registry/base/projects-form-item/projects-form-item.component.ts
@@ -14,23 +14,46 @@ export class RegProjectsFormItem extends RegTagBoxFormItem {
   protected dataSource: any[];
   protected valueExpr: string;
   protected displayExpr: string;
+  protected savedValue: any;
 
   constructor(private ngRedux: NgRedux<IAppState>) {
     super();
+  }
+
+  deserializeValue(value: any): any[] {
+    return value.Project.map(v => +v.ProjectID.__text);
+  }
+
+  serializeValue(value: any[]): any {
+    let index = 0;
+    let projects: any[] = this.savedValue.Project;
+    value.forEach(v => {
+      if (projects.length > index) {
+        projects[index].ProjectID.__text = v.toString();
+      } else {
+        projects.push({ ProjectID: { __text: v.toString() } });
+      }
+      ++index;
+    });
+    while (projects.length > index) {
+      projects.splice(index, 1);
+    }
+    return this.savedValue;
   }
 
   protected update() {
     let lookups = this.ngRedux.getState().session.lookups;
     let options = this.viewModel.editorOptions;
     this.dataSource = lookups ? lookups.projects.filter(i => i.ACTIVE === 'T' && (i.TYPE === 'A' || i.TYPE === options.projectType)) : [];
-    this.value = options && options.value && options.value.Project ? options.value.Project.map(v => +v.ProjectID.__text) : [];
+    this.savedValue = options && options.value ? options.value : { Project: [] };
+    this.value = this.deserializeValue(this.savedValue);
     this.displayExpr = 'NAME';
     this.valueExpr = 'PROJECTID';
   }
 
   protected onValueChanged(e, d) {
     if (e.previousValue !== e.value) {
-      let value = { Project: e.value ? e.value.map(v => { return { ProjectID: { __text: v.toString() } }; }) : [] };
+      let value = this.serializeValue(e.value);
       d.component.option('formData.' + d.dataField, value);
       this.onValueUpdated(this);
     }
