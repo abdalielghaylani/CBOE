deafb1eda927cbc34f6e00633f1d4f0276d65e9a
churlohx <churl.oh@perkinelmer.com>
2017-08-22 14:16:03 -0400

    Implement project update from XML properly

--- subprojects/asp.net/Registration/PresentationTier/Registration.Server/Code/Extensions.cs
+++ subprojects/asp.net/Registration/PresentationTier/Registration.Server/Code/Extensions.cs
@@ -1,10 +1,24 @@
-﻿using System.Xml;
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Xml;
 using CambridgeSoft.COE.Registration.Services.Types;
 
 namespace PerkinElmer.COE.Registration.Server.Code
 {
     public static class Extensions
     {
+        private static void UpdateFromXml(object obj, XmlNode node)
+        {
+            var args = new object[] { node };
+            obj.GetType().GetMethod("UpdateFromXml", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(obj, args);
+        }
+
+        private static void RemoveItem(object obj, int index)
+        {
+            var args = new object[] { index };
+            obj.GetType().GetMethod("RemoveItem", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(obj, args);
+        }
+
         public static void UpdateFromXmlEx(this RegistryRecord record, string xml)
         {
             var doc = new XmlDocument();
@@ -26,27 +40,55 @@ namespace PerkinElmer.COE.Registration.Server.Code
         public static void UpdateFromXmlEx(this PropertyList list, XmlNode dataNode)
         {
             if (dataNode == null) return;
-
+            UpdateFromXml(list, dataNode);
         }
 
         public static void UpdateFromXmlEx(this ProjectList list, XmlNode dataNode)
         {
             if (dataNode == null) return;
+            var nodes = dataNode.SelectNodes("Project");
+            var itemsToRemove = new List<int>();
+            var index = 0;
+            foreach (var p in list)
+            {
+                XmlNode matchingChild = dataNode.SelectSingleNode(string.Format("Project[ID='{0}']", p.ID));
+                // If a node matches this ID, we should update the matching object.
+                if (matchingChild != null)
+                    UpdateFromXml(p, matchingChild);
+                else
+                    itemsToRemove.Insert(0, index);
+                ++index;
+            }
+            foreach (var itemIndex in itemsToRemove)
+            {
+                RemoveItem(list, itemIndex);
+            }
+            foreach (XmlNode node in nodes)
+            {
+                XmlNode idNode = node.SelectSingleNode("ID");
+                if (idNode == null || idNode.InnerText == "0" || idNode.InnerText == string.Empty)
+                {
+                    list.Add(Project.NewProject(node.OuterXml, false, true));
+                }
+            }
         }
 
         public static void UpdateFromXmlEx(this IdentifierList list, XmlNode dataNode)
         {
             if (dataNode == null) return;
+            UpdateFromXml(list, dataNode);
         }
 
         public static void UpdateFromXmlEx(this BatchList list, XmlNode dataNode)
         {
             if (dataNode == null) return;
+            UpdateFromXml(list, dataNode);
         }
 
         public static void UpdateFromXmlEx(this ComponentList list, XmlNode dataNode)
         {
             if (dataNode == null) return;
+            UpdateFromXml(list, dataNode);
         }
     }
 }
