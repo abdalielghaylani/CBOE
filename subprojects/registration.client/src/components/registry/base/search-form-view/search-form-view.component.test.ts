a2750cf2e31cd69b4f2dd44b9533384cac8a9fda
Nithin Krishnan <nithin.krishnan@perkinelmer.com>
2017-09-15 11:47:52 +0530

    Unit test Cases implemented for: - structure-query-form-item - search-fomr-view

---
+++ subprojects/registration.client/src/components/registry/base/search-form-view/search-form-view.component.test.ts
@@ -0,0 +1,65 @@
+import { RegSearchFormView } from './search-form-view.component';
+import { TestBed, async, inject } from '@angular/core/testing';
+import { TestModule } from '../../../../test/test.module';
+import { DevExtremeModule } from 'devextreme-angular';
+import { RegDateFormItem, RegDropDownFormItem, RegTextFormItem, RegDropDownColumnItem, RegStructureColumnItem } from '../../../index';
+import { RegProjectsFormItem, RegFragmentsFormItem, RegIdListFormItem } from '../registry-base.module';
+import { RegStructureQueryFormItem } from '../structure-query-form-item/structure-query-form-item.component';
+import { CommandDropdown } from '../../../../common/tool/command-dropdown.component';
+import { RegStructureQueryOptions } from '../structure-query-form-item/structure-query-form-options.component';
+import { CommandButton } from '../../../../common/tool/command-button.component';
+
+describe('Component : Search Form View', () => {
+
+  let fixture;
+
+  beforeEach(done => {
+    const configure = (testBed: TestBed) => {
+      testBed.configureTestingModule({
+        imports: [ TestModule, DevExtremeModule ],
+        declarations : [ RegSearchFormView, RegDateFormItem, 
+          RegProjectsFormItem, RegFragmentsFormItem, RegIdListFormItem,
+          RegDropDownFormItem, RegStructureQueryFormItem, RegTextFormItem,
+          RegDropDownColumnItem, CommandDropdown, RegStructureQueryOptions, 
+          CommandButton, RegStructureColumnItem ],
+      });
+    };
+
+    TestModule.configureTests(configure).then(testBed => {
+      fixture = testBed.createComponent(RegSearchFormView);
+      fixture.detectChanges();
+      done();
+    });
+  });
+
+  it('Check Component is created for Search Form View', async(inject([], () => {
+    fixture.whenStable().then(() => {
+      fixture.autoDetectChanges();
+      expect(fixture.componentInstance).toBeDefined();
+    });
+  })));
+
+  it('should check if values updated on Field data chnaged event call', async(inject([], () => {
+    fixture.whenStable().then(() => {
+      spyOn(fixture.componentInstance, 'onValueUpdated');
+      fixture.autoDetectChanges();
+      let testEvent = { 'data' : 'Test Data', 'value' : { 'structureCriteriaOptions' : { 'option' : 'TestOption 1' } } };
+      fixture.componentInstance.onFieldDataChanged(testEvent);
+      expect(fixture.componentInstance.onValueUpdated).toHaveBeenCalled();
+    });
+  })));
+
+  it('should check event is emitted on value updated method call', async(inject([], () => {
+    fixture.whenStable().then(() => {
+      fixture.autoDetectChanges();
+      let testEvent = { 'data' : 'Test Data' };
+      fixture.componentInstance.onValueUpdated(testEvent);
+      fixture.componentInstance.valueUpdated.subscribe( e => expect(e).toEqual(testEvent));
+    });
+  })));
+
+  
+
+  // Need to implement test cases for:
+  // 1) on value updated method if condition workflow
+});
--- subprojects/registration.client/src/components/registry/base/structure-query-form-item/structure-query-form-item.component.test.ts
+++ subprojects/registration.client/src/components/registry/base/structure-query-form-item/structure-query-form-item.component.test.ts
@@ -1,10 +1,10 @@
 import { RegStructureQueryFormItem } from './structure-query-form-item.component';
+import { DevExtremeModule } from 'devextreme-angular';
+import { CommandDropdown } from '../../../../common/tool/command-dropdown.component';
+import { CommandButton } from '../../../../common/tool/command-button.component';
+import { RegStructureQueryOptions } from './structure-query-form-options.component';
 import { TestBed, async, inject } from '@angular/core/testing';
 import { TestModule } from '../../../../test/test.module';
-import { RegStructureQueryOptions } from './structure-query-form-options.component';
-import { DevExtremeModule } from 'devextreme-angular';
-import { CommandButton } from '../../../../common/tool/command-button.component';
-import { CommandDropdown } from '../../../../common/tool/command-dropdown.component';
 
 describe('Component : Structure Query Form Item', () => {
 
@@ -14,8 +14,8 @@ describe('Component : Structure Query Form Item', () => {
     const configure = (testBed: TestBed) => {
       testBed.configureTestingModule({
         imports: [ TestModule, DevExtremeModule ],
-        declarations : [ RegStructureQueryFormItem, RegStructureQueryOptions,
-          CommandButton, CommandDropdown ],
+        declarations : [ RegStructureQueryFormItem, CommandDropdown,
+          CommandButton, RegStructureQueryOptions ],
       });
     };
 
@@ -26,10 +26,19 @@ describe('Component : Structure Query Form Item', () => {
     });
   });
 
-  it('Structure Query Form Item component', async(inject([], () => {
+  it('should instantiate Structure Query Form Item component', async(inject([], () => {
     fixture.whenStable().then(() => {
       fixture.autoDetectChanges();
       expect(fixture.componentInstance).toBeDefined();
+    });
+  })));
+
+  it('should check index on viewOptions', async(inject([], () => {
+    fixture.whenStable().then(() => {
+      fixture.autoDetectChanges();
+      let testEvent = { 'data' : 'Test Data' };
+      fixture.componentInstance.viewOptions(testEvent);
+      expect(fixture.componentInstance.index).toEqual(testEvent);
     });
   })));
 
