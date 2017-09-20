44416acac7bc6ca7769eb95e76530989ff25b091
Nithin Krishnan <nithin.krishnan@perkinelmer.com>
2017-09-20 14:58:10 +0530

    Integrated Unit Tests for: - form group view component

---
+++ subprojects/registration.client/src/components/registry/base/form-group-view/form-group-view.component.test.ts
@@ -0,0 +1,60 @@
+import { RegFormGroupView } from './form-group-view.component';
+import { TestModule } from '../../../../test/test.module';
+import { TestBed, async, inject } from '@angular/core/testing';
+import { RegFormGroupItemView } from '../form-group-item-view/form-group-item-view.component';
+import { RegFormView, RegProjectsFormItem, RegFragmentsFormItem, RegIdListFormItem, RegStructureFormItem } from '../registry-base.module';
+import { DevExtremeModule } from 'devextreme-angular';
+import {
+  RegDateFormItem, RegDropDownFormItem, RegStructureImageFormItem, RegTextFormItem,
+  RegDropDownColumnItem, RegStructureColumnItem
+} from '../../../index';
+import { NgReduxModule } from '@angular-redux/store/lib';
+
+describe('Component : Form Group Item View', () => {
+
+  let fixture;
+
+  beforeEach(done => {
+    const configure = (testBed: TestBed) => {
+      testBed.configureTestingModule({
+        imports: [TestModule, DevExtremeModule, NgReduxModule],
+        declarations: [RegFormGroupView, RegFormGroupItemView, RegFormView, RegDateFormItem,
+          RegProjectsFormItem, RegFragmentsFormItem, RegIdListFormItem, RegDropDownFormItem,
+          RegStructureFormItem, RegStructureImageFormItem, RegTextFormItem, RegDropDownColumnItem,
+          RegStructureColumnItem],
+      });
+    };
+
+    TestModule.configureTests(configure).then(testBed => {
+      fixture = testBed.createComponent(RegFormGroupView);
+      fixture.detectChanges();
+      done();
+    });
+  });
+
+  it('should create Form group item view Component', async(inject([], () => {
+    fixture.whenStable().then(() => {
+      fixture.autoDetectChanges();
+      expect(fixture.componentInstance).toBeDefined();
+    });
+  })));
+
+  it('should check values on ngOnChanges method call', async(inject([], () => {
+    fixture.whenStable().then(() => {
+      spyOn(fixture.componentInstance, 'update');
+      fixture.autoDetectChanges();
+      fixture.componentInstance.ngOnChanges();
+      expect(fixture.componentInstance.update).toHaveBeenCalled();
+    });
+  })));
+
+  it('should check value emitted on value updated', async(inject([], () => {
+    fixture.whenStable().then(() => {
+      fixture.autoDetectChanges();
+      fixture.componentInstance.valueUpdated.subscribe(e => expect(e).toEqual(fixture.componentInstance));
+    });
+  })));
+
+  // To do Unit Test for
+  // validate()
+});
--- subprojects/registration.client/src/components/registry/base/structure-query-form-item/structure-query-form-item.component.test.ts
+++ subprojects/registration.client/src/components/registry/base/structure-query-form-item/structure-query-form-item.component.test.ts
@@ -5,6 +5,7 @@ import { CommandButton } from '../../../../common/tool/command-button.component'
 import { RegStructureQueryOptions } from './structure-query-form-options.component';
 import { TestBed, async, inject } from '@angular/core/testing';
 import { TestModule } from '../../../../test/test.module';
+import { StructQueryTestData } from './structure-query-form-item.component.data.test';
 
 describe('Component : Structure Query Form Item', () => {
 
@@ -39,6 +40,42 @@ describe('Component : Structure Query Form Item', () => {
       let testEvent = { 'data' : 'Test Data' };
       fixture.componentInstance.viewOptions(testEvent);
       expect(fixture.componentInstance.index).toEqual(testEvent);
+    });
+  })));
+
+  it('should check values on onValueUpdated method call', async(inject([], () => {
+    fixture.whenStable().then(() => {
+      spyOn(fixture.componentInstance, 'setStructureSearchOptions');
+      fixture.autoDetectChanges();
+      let testEvent = { 'data' : 'Test Data' };
+      fixture.componentInstance.viewModel = undefined;
+      fixture.componentInstance.onValueUpdated(testEvent);
+      expect(fixture.componentInstance.setStructureSearchOptions).toHaveBeenCalled();
+      fixture.componentInstance.valueUpdated.subscribe(e => expect(e).toEqual(fixture.componentInstance));
+    });
+  })));
+
+  it('should check values set using setStructureSearchOptions method', async(inject([], () => {
+    fixture.whenStable().then(() => {
+      // spyOn(fixture.componentInstance, 'setStructureSearchOptions');
+      fixture.autoDetectChanges();
+      fixture.componentInstance.queryModel = StructQueryTestData.QueryModelTestData1;
+      fixture.componentInstance.setStructureSearchOptions();
+      expect(fixture.componentInstance.structureCriteriaOptions._hitAnyChargeHetero).toEqual(StructQueryTestData.structCriteriaOptionsYes);
+      expect(fixture.componentInstance.structureCriteriaOptions._reactionCenter).toEqual(StructQueryTestData.structCriteriaOptionsYes);
+      expect(fixture.componentInstance.structureCriteriaOptions._hitAnyChargeCarbon).toEqual(StructQueryTestData.structCriteriaOptionsYes);
+      expect(fixture.componentInstance.structureCriteriaOptions._permitExtraneousFragments).toEqual(StructQueryTestData.structCriteriaOptionsNo);
+      expect(fixture.componentInstance.structureCriteriaOptions._permitExtraneousFragmentsIfRXN).toEqual(StructQueryTestData.structCriteriaOptionsNo);
+      expect(fixture.componentInstance.structureCriteriaOptions._fragmentsOverlap).toEqual(StructQueryTestData.structCriteriaOptionsNo);
+      expect(fixture.componentInstance.structureCriteriaOptions._tautometer).toEqual(StructQueryTestData.structCriteriaOptionsNo);
+      expect(typeof fixture.componentInstance.structureCriteriaOptions._simThreshold).toEqual('string');
+      expect(fixture.componentInstance.structureCriteriaOptions._simThreshold).toEqual(StructQueryTestData.QueryModelTestData1.simThreshold.toString());
+      expect(fixture.componentInstance.structureCriteriaOptions._fullSearch).toEqual(StructQueryTestData.structCriteriaOptionsNo);
+      expect(fixture.componentInstance.structureCriteriaOptions._identity).toEqual(StructQueryTestData.structCriteriaOptionsNo);
+      expect(fixture.componentInstance.structureCriteriaOptions._similar).toEqual(StructQueryTestData.structCriteriaOptionsNo);
+      expect(fixture.componentInstance.structureCriteriaOptions._tetrahedralStereo).toEqual(StructQueryTestData.QueryModelTestData1.tetrahedralStereo);
+      expect(fixture.componentInstance.structureCriteriaOptions._relativeTetStereo).toEqual(StructQueryTestData.structCriteriaOptionsNo);
+      expect(fixture.componentInstance.structureCriteriaOptions._doubleBondStereo).toEqual(StructQueryTestData.structCriteriaOptionsYes);
     });
   })));
 
