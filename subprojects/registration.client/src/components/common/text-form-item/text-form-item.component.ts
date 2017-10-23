6de979d6c158f65e45534b1b8890593dc13f1781
SHIJU <shijumon.kulangara@perkinelmer.com>
2017-10-23 03:40:16 -0400

    CBOECBOE-5971 (#348)
    Validation Rule: The required field value is not updated with default value text for existing records

--- subprojects/registration.client/src/components/common/text-form-item/text-form-item.component.ts
+++ subprojects/registration.client/src/components/common/text-form-item/text-form-item.component.ts
@@ -8,4 +8,22 @@ import { RegBaseFormItem } from '../base-form-item';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RegTextFormItem extends RegBaseFormItem {
+
+  protected update() {
+    let options = this.viewModel.editorOptions;
+    // set default value
+    let isDefaultValueSet: boolean = false;
+    if (!options.value) {
+      if (this.editMode && options.defaultValue) {
+        options.value = options.defaultValue;
+        isDefaultValueSet = true;
+      }
+    }
+    this.value = options && options.value ? this.deserializeValue(options.value) : undefined;
+
+    // initialized default value, so update view model also
+    if (isDefaultValueSet) {
+      this.updateViewModel();
+    }
+  }
 };
