acea9d38db49b5cbf68b1db86a574c1633c6b1bf
SHIJU <shijumon.kulangara@perkinelmer.com>
2018-01-22 21:30:48 +0530

    Documentation: Removed obsolete files (#653)
    Documentation: Removed obsolete files

--- Web Registration User Guide/Data/Search.js
+++
@@ -1,1 +0,0 @@
-define({r:0.5,st:'Stem',n:1,t:[0,69],u:['../Content/ELN2014-COE Integration/Add Compound from ACX system to a Reaction Drawing.htm'],s:['+','salt'],p:[['+',38],['checkbox',43],['editing',3],['inventory',87],['performed',46],['samples',35],['the',51]]});
