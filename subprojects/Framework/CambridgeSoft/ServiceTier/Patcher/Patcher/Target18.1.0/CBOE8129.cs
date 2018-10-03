00e40d3db97c12da69fa2f634fea1317e572f8bf
binopki <38281598+binopki@users.noreply.github.com>
2018-10-03 15:10:02 +0530

    CBOE-8129 & CBOE-8160 (#1249)
    * CBOE-8160-Database Wizard icon needs to be removed from CBOE manager home page
    Remedy - Removed the Database Wizard from the configuration file.
    
    * CBOE-8129-Re: ChemACX: ChemACX related buttons are visible even user don't have ACX privileges and application logging out after click on related buttons
    CBOE-8160-Database Wizard icon needs to be removed from CBOE manager home page
    Remedy - created patcher project for CBOE-8129 & CBOE-8160

---
+++ subprojects/Framework/CambridgeSoft/ServiceTier/Patcher/Patcher/Target18.1.0/CBOE8129.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace CambridgeSoft.COE.Patcher
+{
+    class CBOE8129 : BugFixBaseCommand
+    {
+        public override List<string> Fix(List<XmlDocument> forms, List<XmlDocument> dataviews, List<XmlDocument> configurations, XmlDocument objectConfig, XmlDocument frameworkConfig)
+        {
+            List<string> messages = new List<string>();
+            bool errorsInPatch = false;
+
+            XmlNode privilegeAttribute = frameworkConfig.SelectSingleNode("//coeHomeSettings/groups/add[@name='ChemACX']/links/add[@name='Search']");
+
+            if (privilegeAttribute != null)
+            {
+                if (string.IsNullOrEmpty(privilegeAttribute.Attributes["privilege"].Value))
+                {
+                    privilegeAttribute.Attributes["privilege"].Value = "BROWSE_ACX||BUY_ACX";
+                    messages.Add("Privileges added for search tag");
+                }
+                else
+                {
+                    errorsInPatch = true;
+                    messages.Add("Privileges already present for search");
+                }
+            }
+            else
+            {
+                errorsInPatch = true;
+                messages.Add("Search node not available on ACX");
+            }
+
+
+            if (!errorsInPatch && messages.Count != 0)
+            {
+                messages.Add("process succeed!");
+            }
+            else
+            {
+                messages.Add("failed to patch.");
+            }
+
+            return messages;
+        }
+    }
+}
+
---
+++ subprojects/Framework/CambridgeSoft/ServiceTier/Patcher/Patcher/Target18.1.0/CBOE8160.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace CambridgeSoft.COE.Patcher
+{
+    class CBOE8160 : BugFixBaseCommand
+    {
+        public override List<string> Fix(List<XmlDocument> forms, List<XmlDocument> dataviews, List<XmlDocument> configurations, XmlDocument objectConfig, XmlDocument frameworkConfig)
+        {
+            List<string> messages = new List<string>();
+            bool errorsInPatch = false;
+
+            XmlNode databaseAttribute = frameworkConfig.SelectSingleNode("//coeHomeSettings/groups/add[@name='ChemACX']/links/add[@name='DatabaseWizard']");
+
+            if (databaseAttribute != null)
+            {
+                if (databaseAttribute.Attributes["display"].Value == "Database Wizard")
+                {
+                    databaseAttribute.RemoveAll();
+                    messages.Add("Database Wizard tag removed successfully");
+                }
+                else
+                {
+                    errorsInPatch = true;
+                    messages.Add("Database Wizard is not present");
+                }
+            }
+            else
+            {
+                errorsInPatch = true;
+                messages.Add("Database Wizard node not available on ACX");
+            }
+
+
+            if (!errorsInPatch && messages.Count != 0)
+            {
+                messages.Add("process succeed!");
+            }
+            else
+            {
+                messages.Add("failed to patch.");
+            }
+
+            return messages;
+        }
+    }
+}
+
