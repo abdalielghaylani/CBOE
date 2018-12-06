8a84663bb168872bdf045f052baf6a5367cc3265
Martín Llanos <32038748+martinllanosarg@users.noreply.github.com>
2018-12-06 15:16:28 -0300

    CBOE-8306 Reverted (#1377)
    * CBOE-8306 Reverted
    
    * CBOE-8306 Reverted

--- subprojects/Database/Registration/Oracle_Install_Scripts/sql/Patches/Patch 18.1.0/patch.sql
+++ subprojects/Database/Registration/Oracle_Install_Scripts/sql/Patches/Patch 18.1.0/patch.sql
@@ -33,62 +33,10 @@ classname = replace(classname, 'Version=17.1.1.0','Version=18.1.0.0')
 where description = 'Registration';
 
 --CBOE-8264
-update coedataview set coedataview =xmltype(
+update coedb.coedataview set coedataview =xmltype(
 replace((coedataview).getclobval(),'<relationship parentkey="212" childkey="201" parent="209" child="1" jointype="OUTER"/>'
 ,'<relationship parentkey="212" childkey="201" parent="209" child="2" jointype="OUTER"/>')) where id=4004;
 --CBOE-8264. End
-
---CBOE-8306
-
-update COEFORM a set a.COEFORM = (updatexml( xmltype(COEFORM),
-'formGroup/detailsForms/detailsForm/coeForms/coeForm[@id="1002"]/addMode/formElement[@name="SCIENTIST_ID"]/COE:configInfo',
-'<COE:configInfo xmlns:COE="COE.FormGroup">
-    <COE:fieldConfig>
-         <COE:CSSLabelClass>FELabel</COE:CSSLabelClass>
-         <COE:CSSClass>FEDropDownListViewMode</COE:CSSClass>
-         <COE:dropDownItemsSelect />
-         <PickListDomain>3</PickListDomain>
-         <COE:Enable>False</COE:Enable>
-         <COE:ID>SCIENTIST_IDProperty</COE:ID>
-         <COE:AutoPostBack>False</COE:AutoPostBack>
-    </COE:fieldConfig>
-</COE:configInfo>'
-,'xmlns="COE.FormGroup" xmlns:COE="COE.FormGroup"')).getclobval()
- where id = 4010;
- 
-update COEFORM a set a.COEFORM = (updatexml( xmltype(COEFORM),
-'formGroup/detailsForms/detailsForm/coeForms/coeForm[@id="1002"]/editMode/formElement[@name="SCIENTIST_ID"]/COE:configInfo',
-'<COE:configInfo xmlns:COE="COE.FormGroup">
-    <COE:fieldConfig>
-         <COE:CSSLabelClass>FELabel</COE:CSSLabelClass>
-         <COE:CSSClass>FEDropDownListViewMode</COE:CSSClass>
-         <COE:dropDownItemsSelect />
-         <PickListDomain>3</PickListDomain>
-         <COE:Enable>True</COE:Enable>
-         <COE:ID>SCIENTIST_IDProperty</COE:ID>
-         <COE:AutoPostBack>False</COE:AutoPostBack>
-    </COE:fieldConfig>
-</COE:configInfo>'
-,'xmlns="COE.FormGroup" xmlns:COE="COE.FormGroup"')).getclobval()
- where id = 4010;
- 
- update COEFORM a set a.COEFORM = (updatexml( xmltype(COEFORM),
-'formGroup/detailsForms/detailsForm/coeForms/coeForm[@id="1002"]/viewMode/formElement[@name="SCIENTIST_ID"]/COE:configInfo',
-'<COE:configInfo xmlns:COE="COE.FormGroup">
-    <COE:fieldConfig>
-         <COE:CSSLabelClass>FELabel</COE:CSSLabelClass>
-         <COE:CSSClass>FEDropDownListViewMode</COE:CSSClass>
-         <COE:dropDownItemsSelect />
-         <PickListDomain>3</PickListDomain>
-         <COE:Enable>True</COE:Enable>
-         <COE:ID>SCIENTIST_IDProperty</COE:ID>
-         <COE:AutoPostBack>False</COE:AutoPostBack>
-    </COE:fieldConfig>
-</COE:configInfo>'
-,'xmlns="COE.FormGroup" xmlns:COE="COE.FormGroup"')).getclobval()
- where id = 4010;
-
---CBOE-8306 End
 
 commit;
  
