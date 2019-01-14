8e5bc7901ec5b91e4f49282ea174163d549f9633
raviprasad-jogi <raviprasad.jogi@perkinelmer.com>
2019-01-14 17:36:05 +0530

    CBOE-8456: 12.1.3DirectUpgrade: DataLoader EXE: Login fails with unhandled exception error (#1406)
    Issue: The version for registration.core assembly is wrong
    Fix: Updated the version for Registration.core assembly to "18.1.1.0"

---
+++ subprojects/Framework/CambridgeSoft/ServiceTier/Patcher/Patcher/Target18.1.1/CBOE8456.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace CambridgeSoft.COE.Patcher
+{
+	class CBOE8456:BugFixBaseCommand
+	{
+
+        public override List<string> Fix(List<XmlDocument> forms, List<XmlDocument> dataviews, List<XmlDocument> configurations, XmlDocument objectConfig, XmlDocument frameworkConfig)
+        {
+            List<string> messages = new List<string>();
+            bool errorsInPatch = false;
+
+            XmlNode COERegistration = frameworkConfig.SelectSingleNode("/configuration/coeConfiguration/services/add[@name='COERegistration']");
+            XmlNode COERegistrationAdmin = frameworkConfig.SelectSingleNode("/configuration/coeConfiguration/services/add[@name='COERegistrationAdmin']");
+
+            if (COERegistration != null)
+            {
+                if (COERegistration.Attributes["dalProviderAssemblyNameFull"].Value == "Registration.Core, Version=12.1.0.0, Culture=neutral, PublicKeyToken=f435ba95da9797dc")
+                {
+                    COERegistration.Attributes["dalProviderAssemblyNameFull"].Value = "Registration.Core, Version=18.1.1.0, Culture=neutral, PublicKeyToken=f435ba95da9797dc";
+                    messages.Add("Registration.Core Assembly Version updated successfully for COERegistration Tag");
+                }
+                else
+                {
+                    messages.Add("Registration.Core Assembly Version is already updated for COERegistration Tag");
+                }
+            }
+            else
+            {
+                errorsInPatch = true;
+                messages.Add("COERegistration tag is not available in FramewrokConfig XML");
+            }
+
+            if (COERegistrationAdmin != null)
+            {
+                if (COERegistrationAdmin.Attributes["dalProviderAssemblyNameFull"].Value == "Registration.Core, Version=12.1.0.0, Culture=neutral, PublicKeyToken=f435ba95da9797dc")
+                {
+                    COERegistrationAdmin.Attributes["dalProviderAssemblyNameFull"].Value = "Registration.Core, Version=18.1.1.0, Culture=neutral, PublicKeyToken=f435ba95da9797dc";
+                    messages.Add("Registration.Core Assembly Version updated successfully for COERegistrationAdmin Tag");
+                }
+                else
+                {
+                    messages.Add("Registration.Core Assembly Version is already updated for COERegistrationAdmin Tag");
+                }
+            }
+            else
+            {
+                errorsInPatch = true;
+                messages.Add("COERegistrationAdmin tag is not available in FramewrokConfig XML");
+            }
+
+
+            if (!errorsInPatch && messages.Count != 0)
+            {
+                messages.Add("process succeed!");
+            }
+            else
+            {
+                messages.Add("failed to patch.");
+            }
+
+            return messages;
+        }
+	}
+}
