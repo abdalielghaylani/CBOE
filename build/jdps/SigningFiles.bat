2e596a52230dbc94ff8a1034cf3c6649c02af336
jeevasayan <35484136+jeevasayan@users.noreply.github.com>
2019-03-27 16:22:08 +0530

    CBOE-8653 DataLoader clickonce installation showing warning for the exe execution by Symantec (#1522)

--- build/jdps/SigningFiles.bat
+++ build/jdps/SigningFiles.bat
@@ -1,6 +1,7 @@
 @echo off
 SET SIGNTOOL=%1
 SET WORKSPACE=%2
+SET FOLDER_VERSION_NAME=%3
 call %SIGNTOOL% "%WORKSPACE%\subprojects\DataLoader\bin\Release\CambridgeSoft.COE.DataLoader.Calculation.dll"
 call %SIGNTOOL% "%WORKSPACE%\Subprojects\DataLoader\bin\Release\CambridgeSoft.COE.DataLoader.Common.dll"
 call %SIGNTOOL% "%WORKSPACE%\Subprojects\DataUploader\DataUploader\bin\Release\CambridgeSoft.COE.DataLoader.Core.dll"
@@ -90,4 +91,44 @@ call %SIGNTOOL% "%WORKSPACE%\Subprojects\Registration\PresentationTier\Registrat
 call %SIGNTOOL% "%WORKSPACE%\Subprojects\Registration\PresentationTier\Registration.Server\bin\CambridgeSoft.COE.Registration.Services.dll"
 call %SIGNTOOL% "%WORKSPACE%\Subprojects\Registration\PresentationTier\Registration.Server\bin\CambridgeSoft.COE.RegistrationAdmin.Services.dll"
 call %SIGNTOOL% "%WORKSPACE%\Subprojects\Registration\PresentationTier\Registration.Server\bin\CambridgeSoft.COE.RegistrationWebApp.dll"
-call %SIGNTOOL% "%WORKSPACE%\Subprojects\Registration\PresentationTier\Registration.Server\bin\Registration.Core.dll"						
+call %SIGNTOOL% "%WORKSPACE%\Subprojects\Registration\PresentationTier\Registration.Server\bin\Registration.Core.dll"
+call %SIGNTOOL% "%WORKSPACE%\Subprojects\DataLoader\bin\Release\app.publish\Application Files\DataLoader_%FOLDER_VERSION_NAME%\CambridgeSoft.COE.Framework.dll.deploy"
+call %SIGNTOOL% "%WORKSPACE%\Subprojects\DataLoader\bin\Release\app.publish\Application Files\DataLoader_%FOLDER_VERSION_NAME%\CambridgeSoft.COE.Framework.XmlSerializers.dll.deploy"
+call %SIGNTOOL% "%WORKSPACE%\Subprojects\DataLoader\bin\Release\app.publish\Application Files\DataLoader_%FOLDER_VERSION_NAME%\CambridgeSoft.COE.Registration.RegistrationAddins.dll.deploy"
+call %SIGNTOOL% "%WORKSPACE%\Subprojects\DataLoader\bin\Release\app.publish\Application Files\DataLoader_%FOLDER_VERSION_NAME%\CambridgeSoft.COE.Registration.Services.dll.deploy"
+call %SIGNTOOL% "%WORKSPACE%\Subprojects\DataLoader\bin\Release\app.publish\Application Files\DataLoader_%FOLDER_VERSION_NAME%\Registration.Core.dll.deploy"
+call %SIGNTOOL% "%WORKSPACE%\subprojects\DataLoader\bin\Release\app.publish\Application Files\DataLoader_%FOLDER_VERSION_NAME%\CambridgeSoft.COE.DataLoader.Calculation.dll.deploy"
+call %SIGNTOOL% "%WORKSPACE%\Subprojects\DataLoader\bin\Release\app.publish\Application Files\DataLoader_%FOLDER_VERSION_NAME%\CambridgeSoft.COE.DataLoader.Common.dll.deploy"
+call %SIGNTOOL% "%WORKSPACE%\Subprojects\DataLoader\bin\Release\app.publish\Application Files\DataLoader_%FOLDER_VERSION_NAME%\DataLoader.exe.deploy"
+call %SIGNTOOL% "%WORKSPACE%\Subprojects\Framework\COELogViewer\app.publish\Application Files\COELogViewer_1_0_0_0\CambridgeSoft.COE.Framework.dll.deploy"
+call %SIGNTOOL% "%WORKSPACE%\Subprojects\Framework\COELogViewer\app.publish\Application Files\COELogViewer_1_0_0_0\CambridgeSoft.COE.Framework.XmlSerializers.dll.deploy"
+call %SIGNTOOL% "%WORKSPACE%\Subprojects\Framework\COELogViewer\app.publish\Application Files\COELogViewer_1_0_0_0\COELogViewer.exe.deploy"
+call %SIGNTOOL% "%WORKSPACE%\Subprojects\Framework\COEPatchingTool\app.publish\Application Files\COEPatcher_1_0_0_0\COEPatcher.exe.deploy"
+call %SIGNTOOL% "%WORKSPACE%\Subprojects\Framework\Framework\ConfigLoader\app.publish\Application Files\ConfigLoader_1_0_0_0\CambridgeSoft.COE.Framework.dll.deploy"
+call %SIGNTOOL% "%WORKSPACE%\Subprojects\Framework\Framework\ConfigLoader\app.publish\Application Files\ConfigLoader_1_0_0_0\ConfigLoader.exe.deploy"
+call %SIGNTOOL% "%WORKSPACE%\Subprojects\Framework\RuntimeLibraries\MolServer12.dll
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\Common\dlls\Asc67Cvt.dll
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\Common\dlls\Base64.dll
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\Common\dlls\Base64Decode.dll
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\Common\dlls\ChemImp.dll
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\Common\dlls\ChemImp.exe
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\Common\dlls\COEHeaderServer.dll
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\Common\dlls\CSDO12.dll
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\Common\dlls\CSSecurityLDAP.dll
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\Common\dlls\ENLDAPProxy.dll
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\Common\dlls\HillOrder.dll
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\Common\dlls\LDAPTest.exe
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\Common\dlls\ReadBinFile.dll
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\Common\dlls\SingleSignOnCom.XmlSerializers.dll
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\ChemInv\config\oracle_install_scripts\Create_test_ChemInv_DB\OracleSetup.exe
+call %SIGNTOOL% "%WORKSPACE%\ChemInv\config\oracle_install_scripts\Export_ChemInv_DB\OracleSetup.exe
+call %SIGNTOOL% "%WORKSPACE%\ChemInv\config\oracle_install_scripts\Create_blank_ChemInv_DB\OracleSetup.exe
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\WebServer_Source\cfserver_scripts\LockBroker.exe
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\WebServer_Source\cfserver_scripts\WAITFOR.DLL
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\WebServer_Source\cfserver_scripts\Lockit.dll
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\WebServer_Source\cfserver_scripts\CDXLibCOM.dll
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\WebServer_Source\cfserver_scripts\Base64Decode.dll
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\WebServer_Source\cfserver_scripts\configiis.exe
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\WebServer_Source\cfserverasp\Clients\SendToEnotebookV1\SendToE-Notebook 18.1.exe
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\WebServer_Source\cfserverasp\Clients\BioAssayClient10.1.6\setup.exe
+call %SIGNTOOL% "%WORKSPACE%\ChemOffice\WebServer_Source\cfserver_scripts\LockBroker.exe				
