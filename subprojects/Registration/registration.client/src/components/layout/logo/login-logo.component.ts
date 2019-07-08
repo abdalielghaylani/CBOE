e1dcb02a73dd5e840882ec38060e434b84930553
Sreejith sahadevan <sreejith.sahadevan@perkinelmer.com>
2019-07-08 14:06:00 +0530

    CBOE-4956 Clicking on PerkinElmer Logo in the login page open the PKI website at same browser, better this should be open in new browse tab (#1645)

--- subprojects/Registration/registration.client/src/components/layout/logo/login-logo.component.ts
+++ subprojects/Registration/registration.client/src/components/layout/logo/login-logo.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'reg-login-logo',
   styles: [require('./logo.css')],
-  template: `<div><a href="http://www.perkinelmer.com/">
+  template: `<div><a href="http://www.perkinelmer.com/" target="_blank">
         <span><img
           class="loginlogo"
           [src]="LogoImage"
