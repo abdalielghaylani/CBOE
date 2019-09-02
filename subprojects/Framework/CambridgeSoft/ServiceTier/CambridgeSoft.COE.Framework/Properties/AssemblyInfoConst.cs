b17f086471804b2d53cc7d23ff8793aec9a5f04d
RaviPrasad <raviprasad.jogi@perkinelmer.com>
2019-09-02 04:55:45 -0700

    CBOE-9124: Moving CBOE projects to Visual Studio 2017 with out New Registration (#1691)
    Issue: Qualification of VS 2017 for CBOE projects
    Fix: Qualification done by changing the solution and fixing the errors occurred

---
+++ subprojects/Framework/CambridgeSoft/ServiceTier/CambridgeSoft.COE.Framework/Properties/AssemblyInfoConst.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CambridgeSoft.COE.Framework.Common
+{
+    public static class AssemblyInfoConst
+    {
+        public const string Next_B = "CambridgeSoft.COE.Framework.ServerControls.PagerControl.Next_B.png";
+        public const string Previous_B = "CambridgeSoft.COE.Framework.ServerControls.PagerControl.Previous_B.png";
+        public const string First_Track_1 = "CambridgeSoft.COE.Framework.ServerControls.PagerControl.First_Track_1.png";
+        public const string Last_Track_1 = "CambridgeSoft.COE.Framework.ServerControls.PagerControl.Last_Track_1.png";
+        public const string Next_B_d = "CambridgeSoft.COE.Framework.ServerControls.PagerControl.Next_B_d.png";
+        public const string Previous_B_d = "CambridgeSoft.COE.Framework.ServerControls.PagerControl.Previous_B_d.png";
+        public const string First_Track_1_d = "CambridgeSoft.COE.Framework.ServerControls.PagerControl.First_Track_1_d.png";
+        public const string Last_Track_1_d = "CambridgeSoft.COE.Framework.ServerControls.PagerControl.Last_Track_1_d.png";
+        public const string InProgress = "CambridgeSoft.COE.Framework.ServerControls.PagerControl.InProgress.gif";
+        public const string Rec_B = "CambridgeSoft.COE.Framework.ServerControls.PagerControl.Rec_B.png";
+        public const string Rec_B_d = "CambridgeSoft.COE.Framework.ServerControls.PagerControl.Rec_B_d.png";
+        public const string error = "CambridgeSoft.COE.Framework.ServerControls.FormGenerator.error.png";
+        public const string DownloadChemDraw = "CambridgeSoft.COE.Framework.ServerControls.FormGenerator.DownloadChemDraw.jpg";
+        public const string ThumbUp = "CambridgeSoft.COE.Framework.ServerControls.FormGenerator.Images.ThumbUp.png";
+        public const string ThumbDown = "CambridgeSoft.COE.Framework.ServerControls.FormGenerator.Images.ThumbDown.png";
+        public const string st_any = "CambridgeSoft.COE.Framework.ServerControls.FormGenerator.Images.st_any.GIF";
+        public const string st_down = "CambridgeSoft.COE.Framework.ServerControls.FormGenerator.Images.st_down.GIF";
+        public const string st_down_thick = "CambridgeSoft.COE.Framework.ServerControls.FormGenerator.Images.st_down_thick.GIF";
+        public const string st_either = "CambridgeSoft.COE.Framework.ServerControls.FormGenerator.Images.st_either.GIF";
+        public const string st_up = "CambridgeSoft.COE.Framework.ServerControls.FormGenerator.Images.st_up.GIF";
+        public const string st_up_thick = "CambridgeSoft.COE.Framework.ServerControls.FormGenerator.Images.st_up_thick.GIF";
+        public const string std_cis = "CambridgeSoft.COE.Framework.ServerControls.FormGenerator.Images.std_cis.GIF";
+        public const string std_either = "CambridgeSoft.COE.Framework.ServerControls.FormGenerator.Images.std_either.GIF";
+        public const string std_trans = "CambridgeSoft.COE.Framework.ServerControls.FormGenerator.Images.std_trans.GIF";
+        public const string disable = "CambridgeSoft.COE.Framework.ServerControls.FormGenerator.Images.disable.png";
+        public const string down = "CambridgeSoft.COE.Framework.ServerControls.FormGenerator.Images.down.png";
+        public const string up = "CambridgeSoft.COE.Framework.ServerControls.FormGenerator.Images.up.png";
+        public const string searching = "CambridgeSoft.COE.Framework.ServerControls.FormGenerator.Images.searching.gif";
+        public const string help = "CambridgeSoft.COE.Framework.ServerControls.FormGenerator.Images.help.png";
+        public const string positioning = "CambridgeSoft.COE.Framework.ServerControls.FormGenerator.JSAndCSS.positioning.js";
+        public const string centerUnMarked = "CambridgeSoft.COE.Framework.ServerControls.MarkHitControls.centerUnMarked.gif";
+        public const string centerMarked = "CambridgeSoft.COE.Framework.ServerControls.MarkHitControls.centerMarked.gif";
+        public const string right = "CambridgeSoft.COE.Framework.ServerControls.MarkHitControls.right.gif";
+        public const string left = "CambridgeSoft.COE.Framework.ServerControls.MarkHitControls.left.gif";
+        public const string Warning_h = "CambridgeSoft.COE.Framework.ServerControls.ErrorControl.Images.Warning_h.png";
+        public const string export_btn = "CambridgeSoft.COE.Framework.ServerControls.COEExportControl.export_btn.png";
+        public const string cancel_btn = "CambridgeSoft.COE.Framework.ServerControls.COEExportControl.cancel_btn.png";
+        public const string Add_h = "CambridgeSoft.COE.Framework.ServerControls.COEExportControl.Add_h.png";
+        public const string File_Edit = "CambridgeSoft.COE.Framework.ServerControls.COEExportControl.File_Edit.png";
+        public const string Folder = "CambridgeSoft.COE.Framework.ServerControls.COEExportControl.Folder.png";
+        public const string File_Add = "CambridgeSoft.COE.Framework.ServerControls.COEExportControl.File_Add.png";
+        public const string Remove_h = "CambridgeSoft.COE.Framework.ServerControls.COEExportControl.Remove_h.png";
+        public const string resultcriteria = "CambridgeSoft.COE.Framework.ServerControls.COEExportControl.resultcriteria.xml";
+        public const string LiveDataview = "CambridgeSoft.COE.Framework.ServerControls.COEExportControl.LiveDataview.xml";
+        public const string LiveResultCriteria = "CambridgeSoft.COE.Framework.ServerControls.COEExportControl.LiveResultCriteria.xml";
+        public const string Add_Document = "CambridgeSoft.COE.Framework.ServerControls.COEExportControl.Add_Document.png";
+        public const string Arrow_Down_B = "CambridgeSoft.COE.Framework.ServerControls.COEExportControl.Arrow_Down_B.png";
+        public const string Arrow_Up_B = "CambridgeSoft.COE.Framework.ServerControls.COEExportControl.Arrow_Up_B.png";
+        public const string Block = "CambridgeSoft.COE.Framework.ServerControls.COEExportControl.Block.png";
+        public const string Check = "CambridgeSoft.COE.Framework.ServerControls.COEExportControl.Check.png";
+        public const string Close_Window = "CambridgeSoft.COE.Framework.ServerControls.COEExportControl.Close_Window.png";
+        public const string Details = "CambridgeSoft.COE.Framework.ServerControls.COEExportControl.Details.png";
+        public const string Edit = "CambridgeSoft.COE.Framework.ServerControls.COEExportControl.Edit.png";
+        public const string Export = "CambridgeSoft.COE.Framework.ServerControls.COEExportControl.Export.png";
+        public const string Save = "CambridgeSoft.COE.Framework.ServerControls.COEExportControl.Save.png";
+        public const string Stop = "CambridgeSoft.COE.Framework.ServerControls.COEExportControl.Stop.png";
+        public const string Styles_PageControl = "CambridgeSoft.COE.Framework.ServerControls.PageControlSettingsManager.Themes.Styles_PageControl.css";
+        public const string file = "CambridgeSoft.COE.Framework.ServerControls.PageControlSettingsManager.Themes.Images.file.gif";
+        public const string folder = "CambridgeSoft.COE.Framework.ServerControls.PageControlSettingsManager.Themes.Images.folder.gif";
+        public const string buttonimage = "CambridgeSoft.COE.Framework.ServerControls.PageControlSettingsManager.Themes.Images.buttonimage.gif";
+        public const string backgroundtoolbar = "CambridgeSoft.COE.Framework.ServerControls.PageControlSettingsManager.Themes.Images.backgroundtoolbar.gif";
+    } 
+}
