2790619de7b3089d688da21cc821ca4150cb22df
COEDEV3\Administrator <louis.kurian@perkinelmer.com>
2019-10-16 02:22:14 -0400

    no message

--- build/jdps/BuildingProjects.bat
+++ build/jdps/BuildingProjects.bat
@@ -1,18 +1,21 @@
 @echo off
 SET MSBUILD_PATH=%1
 SET WORKSPACE=%2
+SET MSBUILD64=%3
 nuget.exe update -self
 nuget.exe restore "%WORKSPACE%\subprojects\Manager\Manager.sln"
 nuget.exe restore "%WORKSPACE%\subprojects\Security\SingleSignOn\SingleSignOn.sln"
-%MSBUILD_PATH% %WORKSPACE%\subprojects\Security\SingleSignOn\SingleSignOn.sln /t:Build /p:Configuration=Release
-%MSBUILD_PATH% %WORKSPACE%\subprojects\Framework\CambridgeSoft\ServiceTier\CambridgeSoft.COE.Framework.sln /t:Build /p:Configuration=Release
-%MSBUILD_PATH% %WORKSPACE%\subprojects\Registration\PresentationTier\RegistrationWebApp.sln  /t:Build /p:Configuration=Release
-%MSBUILD_PATH% %WORKSPACE%\subprojects\DataLoader\CambridgeSoft.COE.DataLoader.sln /t:Build /p:Configuration=Release
-%MSBUILD_PATH% %WORKSPACE%\subprojects\DataLoader\CambridgeSoft.COE.DataLoader.sln /t:Publish /p:Configuration=Release
-%MSBUILD_PATH% %WORKSPACE%\subprojects\ConfigLoader\CambridgeSoft.COE.ConfigLoader.sln /t:Publish /p:Configuration=Release
-%MSBUILD_PATH% %WORKSPACE%\subprojects\ChemBioViz\ServiceTier\CambridgeSoft.COE.ChemBioViz.sln /t:Build /p:Configuration=Release
-%MSBUILD_PATH% %WORKSPACE%\subprojects\Manager\Manager.sln /t:Build /p:Configuration=Release
-%MSBUILD_PATH% %WORKSPACE%\subprojects\DataUploader\DataUploader.sln /t:Build /p:Configuration=Release
-%MSBUILD_PATH% %WORKSPACE%\subprojects\Registration\ServiceTier\RLSConfigurationTool\RLSConfigurationTool.sln /t:Build /p:Configuration=Release
-%MSBUILD_PATH% %WORKSPACE%\subprojects\Framework\CambridgeSoft\ServiceTier\Patcher\Patcher.sln  /t:Publish /p:Configuration=Release
-%MSBUILD_PATH% %WORKSPACE%\subprojects\Framework\CambridgeSoft\ServiceTier\COELogViewer\COELogViewer.sln /t:Publish /p:Configuration=Release	
+%MSBUILD_PATH% %WORKSPACE%\subprojects\Security\SingleSignOn\SingleSignOn.sln /t:Build /p:Configuration=Release /p:Platform=x64
+%MSBUILD64% %WORKSPACE%\subprojects\Framework\CambridgeSoft\ServiceTier\CambridgeSoft.COE.Framework.sln /t:Build /p:Configuration=Release /p:Platform=x64
+%MSBUILD_PATH% %WORKSPACE%\subprojects\Registration\PresentationTier\RegistrationWebApp.sln  /t:Build /p:Configuration=Release /p:Platform=x64
+%MSBUILD_PATH% %WORKSPACE%\subprojects\DataLoader\CambridgeSoft.COE.DataLoader.sln /t:Build /p:Configuration=Release /p:Platform=x64
+%MSBUILD_PATH% %WORKSPACE%\subprojects\DataLoader\CambridgeSoft.COE.DataLoader.sln /t:Publish /p:Configuration=Release /p:Platform=x64
+%MSBUILD_PATH% %WORKSPACE%\subprojects\ConfigLoader\CambridgeSoft.COE.ConfigLoader.sln /t:Publish /p:Configuration=Release /p:Platform=x64
+%MSBUILD_PATH% %WORKSPACE%\subprojects\ChemBioViz\ServiceTier\CambridgeSoft.COE.ChemBioViz.sln /t:Build /p:Configuration=Release /p:Platform=x64
+%MSBUILD_PATH% %WORKSPACE%\subprojects\Manager\Manager.sln /t:Build /p:Configuration=Release /p:Platform=x64
+%MSBUILD_PATH% %WORKSPACE%\subprojects\DataUploader\DataUploader.sln /t:Build /p:Configuration=Release /p:Platform=x64
+%MSBUILD_PATH% %WORKSPACE%\subprojects\Registration\ServiceTier\RLSConfigurationTool\RLSConfigurationTool.sln /t:Build /p:Configuration=Release /p:Platform=x64
+%MSBUILD_PATH% %WORKSPACE%\subprojects\Framework\CambridgeSoft\ServiceTier\Patcher\Patcher.sln  /t:Publish /p:Configuration=Release /p:Platform=x64
+%MSBUILD_PATH% %WORKSPACE%\subprojects\Framework\CambridgeSoft\ServiceTier\COELogViewer\COELogViewer.sln /t:Publish /p:Configuration=Release /p:Platform=x64
+
+
--- build/jdps/CompileAndCopyGoods.bat
+++ build/jdps/CompileAndCopyGoods.bat
@@ -8,8 +8,6 @@ MD "%WORKSPACE%\Install\install9\UserInterface"
 xcopy /Y/S "C:\builds\workspace\CBOE18.1_Pipeline\subprojects\Registration\PresentationTier\Test\*.*" "%WORKSPACE%\subprojects\Registration\PresentationTier\Test\"
 xcopy /Y/S "C:\builds\workspace\CBOE18.1_Pipeline\Install\install9\UserInterface\*.*" "%WORKSPACE%\Install\install9\UserInterface\"
 xcopy /Y/S "C:\builds\workspace\CBOE18.1_Pipeline\ChemOffice\webserver_source\cfserveradmin\AdminSource\mswcrun.dll" "%WORKSPACE%\ChemOffice\webserver_source\cfserveradmin\AdminSource\"
-xcopy /Y "%WORKSPACE%\subprojects\Registration\PresentationTier\Test\Registration.Server.2013.Build.sln"  "%WORKSPACE%\subprojects\Registration\PresentationTier"
-xcopy /Y "%WORKSPACE%\subprojects\Registration\PresentationTier\Test\Registration.Server.csproj"   "%WORKSPACE%\subprojects\Registration\PresentationTier\Registration.Server"
 xcopy /Y "%WORKSPACE%\subprojects\ChemBioViz\ServiceTier\ChemBioViz.Services\bin\Release\CambridgeSoft.COE.ChemBioViz.Services.dll" "%WORKSPACE%\CBOE17.1"
 xcopy /Y "%WORKSPACE%\subprojects\Registration\PresentationTier\RegistrationWebApp\Bin\CambridgeSoft.COE.Framework.dll" "%WORKSPACE%\CBOE17.1"
 xcopy /Y "%WORKSPACE%\subprojects\Registration\PresentationTier\RegistrationWebApp\Bin\CambridgeSoft.COE.Framework.XmlSerializers.dll" "%WORKSPACE%\CBOE17.1"
@@ -20,7 +18,7 @@ xcopy /Y "%WORKSPACE%\subprojects\Registration\PresentationTier\RegistrationWebA
 xcopy /Y "%WORKSPACE%\subprojects\Registration\PresentationTier\RegistrationWebApp\Bin\Registration.Core.dll" "%WORKSPACE%\CBOE17.1" 
 nuget.exe update -self
 nuget.exe restore "%WORKSPACE%\subprojects\Registration\PresentationTier\Registration.Server.2013.Build.sln"
-%MSBUILD% %WORKSPACE%\subprojects\Registration\PresentationTier\Registration.Server.2013.Build.sln /p:CreatePackageOnPublish=true /p:DeployOnBuild=true
+%MSBUILD% %WORKSPACE%\subprojects\Registration\PresentationTier\Registration.Server.2013.Build.sln /p:CreatePackageOnPublish=true /p:DeployOnBuild=true /p:Platform=x64
 xcopy /Y/S "%WORKSPACE%\subprojects\Framework\CambridgeSoft\CommonRuntimeLibraries\*.*" "%WORKSPACE%\subprojects\Registration\PresentationTier\Registration.Server\bin\"
 xcopy /Y/S "%WORKSPACE%\CBOE17.1\*.*" "%WORKSPACE%\subprojects\Registration\PresentationTier\Registration.Server\bin\"
 xcopy /Y/S "%WORKSPACE%\subprojects\ChemBioViz\ServiceTier\ChemBioViz.Services\bin\Release\Interop.ChemDrawControl18.dll" "%WORKSPACE%\subprojects\Registration\PresentationTier\Registration.Server\bin"
