5103bd0723d334cb36f0523905247c38ac6af4e6
UVJ\sreejith.sahadevan <sreejith.sahadevan@perkinelmer.com>
2019-10-24 18:28:39 +0530

    CBOE-9277  Reg UI: Scroll bars are missing in record detailed view

--- subprojects/Registration/registration.client/src/services/cdjs.service.ts
+++ subprojects/Registration/registration.client/src/services/cdjs.service.ts
@@ -10,54 +10,56 @@ declare var document: any;
 @Injectable()
 export class CdjsService {
 
-  private scripts: any = {};
+    private scripts: any = {};
 
-  constructor(private ngRedux: NgRedux<IAppState>) {
-    let lookups = this.ngRedux.getState().session.lookups;
-    this.scripts[cdjsScriptName] = {
-      loaded: false,
-      src: lookups.systemInformation.CDJSUrl + '/js/chemdrawweb/chemdrawweb.js'
-    };
-  }
+    constructor(private ngRedux: NgRedux<IAppState>) {
+        try {
+            let lookups = this.ngRedux.getState().session.lookups;
+            this.scripts[cdjsScriptName] = {
+                loaded: false,
+                src: lookups.systemInformation.CDJSUrl + '/js/chemdrawweb/chemdrawweb.js'
+            };
+        } catch (e) { console.trace(e); }
+    }
 
-  load(scripts: string[]) {
-      let promises: any[] = [];
-      scripts.forEach((script) => promises.push(this.loadScript(script)));
-      return Promise.all(promises);
-  }
+    load(scripts: string[]) {
+        let promises: any[] = [];
+        scripts.forEach((script) => promises.push(this.loadScript(script)));
+        return Promise.all(promises);
+    }
 
-  loadCdjsScript() {
-      this.loadScript(cdjsScriptName)
-        .catch(error => {
-            notifyException(`CDJS script could not be loaded`, error, 5000);
-          });
-  }
+    loadCdjsScript() {
+        this.loadScript(cdjsScriptName)
+            .catch(error => {
+                notifyException(`CDJS script could not be loaded`, error, 5000);
+            });
+    }
 
-  loadScript(name: string) {
-      return new Promise((resolve, reject) => {
-          if (this.scripts[name].loaded) {
-              resolve({script: name, loaded: true, status: 'Already Loaded'});
-          } else {
-              let script = document.createElement('script');
-              script.type = 'text/javascript';
-              script.src = this.scripts[name].src;
-              if (script.readyState) {
-                  script.onreadystatechange = () => {
-                      if (script.readyState === 'loaded' || script.readyState === 'complete') {
-                          script.onreadystatechange = null;
-                          this.scripts[name].loaded = true;
-                          resolve({script: name, loaded: true, status: 'Loaded'});
-                      }
-                  };
-              } else {
-                  script.onload = () => {
-                      this.scripts[name].loaded = true;
-                      resolve({script: name, loaded: true, status: 'Loaded'});
-                  };
-              }
-              script.onerror = (error: any) => reject({script: name, loaded: false, status: 'Error'});
-              document.getElementsByTagName('head')[0].appendChild(script);
-          }
-      });
-  }
+    loadScript(name: string) {
+        return new Promise((resolve, reject) => {
+            if (this.scripts[name].loaded) {
+                resolve({ script: name, loaded: true, status: 'Already Loaded' });
+            } else {
+                let script = document.createElement('script');
+                script.type = 'text/javascript';
+                script.src = this.scripts[name].src;
+                if (script.readyState) {
+                    script.onreadystatechange = () => {
+                        if (script.readyState === 'loaded' || script.readyState === 'complete') {
+                            script.onreadystatechange = null;
+                            this.scripts[name].loaded = true;
+                            resolve({ script: name, loaded: true, status: 'Loaded' });
+                        }
+                    };
+                } else {
+                    script.onload = () => {
+                        this.scripts[name].loaded = true;
+                        resolve({ script: name, loaded: true, status: 'Loaded' });
+                    };
+                }
+                script.onerror = (error: any) => reject({ script: name, loaded: false, status: 'Error' });
+                document.getElementsByTagName('head')[0].appendChild(script);
+            }
+        });
+    }
 }
